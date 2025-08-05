cfe3080b52027f41cc659d52a622b2647b42b00e
manmarina <manmarina@me.com>
2025-08-05 14:52:21 +0900

    318A回数が-にならないように修正

--- C/ABC/ABC318A.c
+++ C/ABC/ABC318A.c
@@ -5,7 +5,15 @@ int main()
     int N, M, P;
     scanf("%d%d%d", &N, &M, &P);
 
-    int count = (N - M) / P + 1;
+    int count;
+    if (M > N)
+    {
+        count = 0;
+    }
+    else
+    {
+        count = (N - M) / P + 1;
+    }
     printf("%d\n", count);
 
     return 0;
--- python/ABC/ABC318A.py
+++ python/ABC/ABC318A.py
@@ -1,4 +1,8 @@
 N ,M, P = map(int, input().split())
 
-count = (N - M) // P + 1
+if M > N:
+    count = 0
+else:
+    count = (N - M) // P + 1
+
 print(count)
