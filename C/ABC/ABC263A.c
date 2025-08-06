039de5fb86ce7fc5fa9f3a6610034e04452688b0
manmarina <manmarina@me.com>
2025-08-06 16:01:57 +0900

    263A AC

--- C/ABC/ABC263A.c
+++ C/ABC/ABC263A.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 
+int compare(const void *a, const void *b)
+{
+    return (*(int *)a - *(int *)b);
+}
+
 int main()
 {
-    int L, R;
-    scanf("%d%d", &L, &R);
+    int S[5];
+    for (int i = 0; i < 5; i++)
+        scanf("%d", &S[i]);
+
+    qsort(S, 5, sizeof(int), compare);
+
+    if ((S[0] == S[1] && S[2] == S[3] && S[3] == S[4]) ||
+        (S[0] == S[1] && S[1] == S[2] && S[3] == S[4]))
+        puts("Yes");
+    else
+        puts("No");
 
     return 0;
 }
--- python/ABC/ABC263A.py
+++ python/ABC/ABC263A.py
@@ -1,12 +1,7 @@
 S = list(map(int, input().split()))
 S.sort()
 
-count = 1
-for i in range(len(S)-1):
-    if S[i] != S[i+1]:
-        count += 1
-
-if count == 2:
+if (S[0] == S[1] and S[2] == S[3] == S[4]) or (S[0] == S[1] == S[2] and S[3] == S[4]):
     print("Yes")
 else:
     print("No")
