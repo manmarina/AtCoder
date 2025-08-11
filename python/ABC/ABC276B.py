2685ab58f970adfcfa27d707acb4a34f74327a75
manmarina <manmarina@me.com>
2025-08-11 22:31:40 +0900

    276B.py cityに直接代入

--- python/ABC/ABC276B.py
+++ python/ABC/ABC276B.py
@@ -1,13 +1,10 @@
 N, M = map(int, input().split())
 
-AB = []
-for i in range(M):
-    AB.append(list(map(int, input().split())))
-
 city = [[] for i in range(N)]
 for i in range(M):
-    city[AB[i][0] - 1].append(AB[i][1])
-    city[AB[i][1] - 1].append(AB[i][0])
+    A, B = map(int, input().split())
+    city[A - 1].append(B)
+    city[B - 1].append(A)
 
 for i in range(N):
     city[i].sort()
