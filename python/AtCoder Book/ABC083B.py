20be92f81ee8b14b4e15fa0c3b85ad02b0a8a5e0
manmarina <manmarina@me.com>
2025-08-13 15:29:43 +0900

    083B 各桁の和の演算を関数に変更

--- python/AtCoder Book/ABC083B.py
+++ python/AtCoder Book/ABC083B.py
@@ -1,13 +1,17 @@
 N, A, B = map(int, input().split())
 
-total = 0
-for i in range(1, N + 1):
+
+def calc_sum(num):
     sum_digit = 0
-    num = i
     while num > 0:
         sum_digit += num % 10
         num //= 10
-    if A <= sum_digit <= B:
+    return sum_digit
+
+
+total = 0
+for i in range(1, N + 1):
+    if A <= calc_sum(i) <= B:
         total += i
 
 print(total)
