c2e536c872f927331b3ea24b0cd41dc83f2454aa
manmarina <manmarina@me.com>
2025-08-15 06:28:46 +0900

    083B テキストによる別解

--- python/AtCoder Book/ABC083B.py
+++ python/AtCoder Book/ABC083B.py
@@ -2,16 +2,13 @@ N, A, B = map(int, input().split())
 
 
 def calc_sum(num):
-    sum_digit = 0
-    while num > 0:
-        sum_digit += num % 10
-        num //= 10
-    return sum_digit
+    return sum(map(int, str(num)))
 
 
-total = 0
-for i in range(1, N + 1):
-    if A <= calc_sum(i) <= B:
-        total += i
+# total = 0
+# for i in range(1, N + 1):
+#     if A <= calc_sum(i) <= B:
+#         total += i
+total = sum(i for i in range(1, N + 1) if A <= calc_sum(i) <= B)
 
 print(total)
