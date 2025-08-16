97938464bfb454e9069310658a27e11b00e27ac1
manmarina <manmarina@me.com>
2025-08-16 10:06:22 +0900

    197B eval()を使い分岐をコントロール

--- python/AtCoder Book/ABC197B.py
+++ python/AtCoder Book/ABC197B.py
@@ -4,48 +4,41 @@ for i in range(H):
     S.append(input())
 
 
+def count_visible_1way(row_count, col_count, x_home, y_home, map, direction):
+    if direction == "right":
+        condition_1 = "y_home + gain > col_count - 1"
+        condition_2 = "map[x_home][y_home + gain]"
+    elif direction == "left":
+        condition_1 = "y_home - gain < 0"
+        condition_2 = "map[x_home][y_home - gain]"
+    elif direction == "upper":
+        condition_1 = "x_home - gain < 0"
+        condition_2 = "map[x_home - gain][y_home]"
+    elif direction == "lower":
+        condition_1 = "x_home + gain > row_count - 1"
+        condition_2 = "map[x_home + gain][y_home]"
+    gain = 1
+    while 1:
+        if eval(condition_1):
+            break
+        if eval(condition_2) == '#':
+            break
+        gain += 1
+    return gain - 1
+
+
 def count_visible(row_count, col_count, x_home, y_home, map):
     x_home -= 1
     y_home -= 1
     directions = ["right", "left", "upper", "lower"]
     count = 1
     for direction in directions:
-        if direction == "right":
-            gain = 1
-            while 1:
-                if y_home + gain > col_count - 1:
-                    break
-                if map[x_home][y_home + count] == '#':
-                    break
-                count += 1
-                gain += 1
-        elif direction == "left":
-            gain = 1
-            while 1:
-                if y_home - gain < 0:
-                    break
-                if map[x_home][y_home - gain] == '#':
-                    break
-                count += 1
-                gain += 1
-        elif direction == "upper":
-            gain = 1
-            while 1:
-                if x_home - gain < 0:
-                    break
-                if map[x_home - gain][y_home] == '#':
-                    break
-                count += 1
-                gain += 1
-        elif direction == "lower":
-            gain = 1
-            while 1:
-                if x_home + gain > row_count - 1:
-                    break
-                if map[x_home + gain][y_home] == '#':
-                    break
-                count += 1
-                gain += 1
+        count += count_visible_1way(row_count,
+                                    col_count,
+                                    x_home,
+                                    y_home,
+                                    map,
+                                    direction)
     return count
 
 
