d71bbcd794b1514c2d6a586012ed8b6129e7c901
manmarina <manmarina@me.com>
2025-08-17 00:37:48 +0900

    051B 関数呼び出し時のtime変化 まさかの高速化

--- python/AtCoder Book/ABC051B.py
+++ python/AtCoder Book/ABC051B.py
@@ -1,10 +1,14 @@
 K, S = map(int, input().split())
 
-count = 0
-for x in range(K + 1):
-    for y in range(K + 1):
-        z = S - x - y
-        if 0 <= z <= K:
-            count += 1
 
-print(count)
+def sum_of_3_integers(K, S):
+    count = 0
+    for x in range(K + 1):
+        for y in range(K + 1):
+            z = S - x - y
+            if 0 <= z <= K:
+                count += 1
+    return count
+
+
+print(sum_of_3_integers(K, S))
