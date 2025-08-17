c443a6b283c7bd283412cab72a2762c149c3ce87
manmarina <manmarina@me.com>
2025-08-17 10:58:50 +0900

    157C 強引にAC

--- python/AtCoder Book/ABC157C.py
+++ python/AtCoder Book/ABC157C.py
@@ -1,28 +1,49 @@
 N, M = map(int, input().split())
 sc = [tuple(map(int, input().split())) for _ in range(M)]
 
-# 各桁の指定値を管理（未指定は None）
-digits = [None] * N
+# print(1, sc)
 
-# 矛盾チェック（同じ桁に違う値が来たらNG）
-for s, c in sc:
-    idx = s - 1
-    if digits[idx] is None:
-        digits[idx] = c
-    elif digits[idx] != c:
-        print(-1)
-        exit()
 
-# 先頭桁チェック（N>1 のときは 0 始まりはNG）
-if N > 1 and digits[0] == 0:
+def is_able_number(N, conditions_list):
+    conditions = set(_ for _ in conditions_list)
+    # print(2, conditions)
+
+    first_digit = []
+    for condition in conditions:
+        # 矛盾チェック（N=1以外の時、先頭桁が0はNG
+        if condition == (1, 0) and N != 1:
+            return None
+        first_digit.append(condition[0])
+
+    # print(3, first_digit)
+
+    # 矛盾チェック（同じ桁に違う値が来たらNG）
+    if len(first_digit) != len(set(first_digit)):
+        return None
+
+    return conditions
+
+
+def make_integer(digit, conditions):
+    integer_list = [0 for i in range(digit)]
+    for condition in conditions:
+        integer_list[condition[0] - 1] = condition[1]
+
+    # 1桁目が0なら1に更新
+    if integer_list[0] == 0 and N != 1:
+        integer_list[0] = 1
+    # print(5, integer_list)
+
+    integer = 0
+    for d in integer_list:
+        integer = integer * 10 + d
+    return integer
+
+
+conditions = is_able_number(N, sc)
+# print(4, conditions)
+
+if conditions is None:
     print(-1)
-    exit()
-
-# 最小値で埋める
-if digits[0] is None:
-    digits[0] = 0 if N == 1 else 1
-for i in range(1, N):
-    if digits[i] is None:
-        digits[i] = 0
-
-print(int("".join(map(str, digits))))
+else:
+    print(make_integer(N, conditions))
