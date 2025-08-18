5070c7ca95797f8141c0db53debadd9ca5529316
manmarina <manmarina@me.com>
2025-08-18 11:54:21 +0900

    118B 連想配列による実装

--- python/AtCoder Book/ABC118B.py
+++ python/AtCoder Book/ABC118B.py
@@ -1,17 +1,17 @@
+from collections import defaultdict
+
 N, M = map(int, input().split())
 
-# 空のバケットを作成
-bucket = [0] * 31
-
-# スキャンしながらバケットに追加
+# スキャンしながら連想配列に追加
+dd = defaultdict(int)
 for i in range(N):
     A = (list(map(int, input().split())))
     for a in A[1:]:
-        bucket[a] += 1
+        dd[a] += 1
 
-# バケットを走査して、カウントがNのものの数を表示
+# 連想配列を走査して、カウントがNのものの数を表示
 count = 0
-for i in bucket:
+for i in dd.values():
     if i == N:
         count += 1
 print(count)
