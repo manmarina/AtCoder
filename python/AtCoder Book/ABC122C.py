967ce918f2645ddf8e5f5688d49f3e107f7d2eed
manmarina <manmarina@me.com>
2025-08-20 15:18:41 +0900

    122C テキストのコード

---
+++ python/AtCoder Book/ABC122C.py
@@ -0,0 +1,13 @@
+N, Q = map(int, input().split())
+S = input()
+
+cs = [0] * (N + 1)
+for i in range(1, N):
+    cs[i + 1] = cs[i] + (S[i - 1:i + 1] == "AC")
+
+for q in range(Q):
+    l, r = map(int, input().split())
+
+    l -= 1
+
+    print(cs[r] - cs[l + 1])
