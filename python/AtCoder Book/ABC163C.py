3a3b7e75321cb365197be42a7c6dcfb69cde02b3
manmarina <manmarina@me.com>
2025-08-20 10:23:35 +0900

    163C グラフによる解法を自力で

--- python/AtCoder Book/ABC163C.py
+++ python/AtCoder Book/ABC163C.py
@@ -1,15 +1,11 @@
 N = int(input())
 A = list(map(int, input().split()))
 
-# 子リスト（部下リスト）
-children = [[] for _ in range(N)]
+G = [[] for i in range(N)]
 
-# 社員 2..N の上司が A[i] なので辺を張る
-for i, boss in enumerate(A, start=2):  # i=社員番号
-    children[boss - 1].append(i - 1)       # boss, i は 0-index に変換
+for i, v in enumerate(A):
+    i += 1
+    v -= 1
+    G[v].append(i)
 
-# 部下人数を数える
-for i in range(N):
-    print(len(children[i]))
-
-# print(children)
+print(*map(len, G), sep='\n')
