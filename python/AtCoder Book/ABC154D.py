248a5b750a61f70d096efe4e323535fc9db2cf79
manmarina <manmarina@me.com>
2025-08-21 09:02:20 +0900

    154D 等差数列の和の平均の計算を改良

--- python/AtCoder Book/ABC154D.py
+++ python/AtCoder Book/ABC154D.py
@@ -7,17 +7,22 @@ cs = [0] * (MAX_NUM + 1)
 
 
 def calc_ev(n):
-    ev = 1 / n * (sum(i for i in range(1, n + 1)))
+    # ev = 1 / n * (sum(i for i in range(1, n + 1)))
+    ev = (n + 1) / 2
     return ev
 
 
-mx = 0
 for i, v in enumerate(p, 1):
     ev = calc_ev(v)
     cs[i] = cs[i - 1] + ev
-    if i > K - 1:
-        temp = cs[i] - cs[i - K]
-        mx = max(mx, temp)
+
+mx = 0
+for i in range(len(p) - K + 1):
+    l = i
+    r = i + K
+    temp = cs[r] - cs[l]
+    mx = max(mx, temp)
+
 
 # print(cs)
 print(mx)
