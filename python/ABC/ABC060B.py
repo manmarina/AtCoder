03d15105381260a663c7ea48fe2865b0a6b28a6b
manmarina <manmarina@me.com>
2025-08-24 21:47:04 +0900

    060B チャッピー シミュレーションによる実装

--- python/ABC/ABC060B.py
+++ python/ABC/ABC060B.py
@@ -1,4 +1,12 @@
-import math
-
 A, B, C = map(int, input().split())
-print("YES" if C % math.gcd(A, B) == 0 else "NO")
+
+seen = [False] * B
+x = 0  # 0 から A を足し込んでいくと (A*i) % B を順に辿れる
+while not seen[x]:
+    if x == C:
+        print("YES")
+        break
+    seen[x] = True
+    x = (x + A) % B
+else:
+    print("NO")
