e1b475c796bdc750ea1257136e376d3b51f20860
manmarina <manmarina@me.com>
2025-08-25 10:36:35 +0900

    079B チャッピー リストを使わない解法

--- python/ABC/ABC079B.py
+++ python/ABC/ABC079B.py
@@ -1,8 +1,6 @@
 N = int(input())
 
-L = [2, 1]
-for i in range(2, 86 + 1):
-    i = L[i - 1] + L[i - 2]
-    L.append(i)
-
-print(L[N])
+a, b = 2, 1        # a=L0, b=L1
+for _ in range(N):
+    a, b = b, a + b
+print(a)            # ループ後のaがL_N
