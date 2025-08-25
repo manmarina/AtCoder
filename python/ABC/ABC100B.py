5c07906d7fe78a75f188c964bb2fd9903b2abd75
manmarina <manmarina@me.com>
2025-08-25 18:16:53 +0900

    100B チャッピー 全探索による解

--- python/ABC/ABC100B.py
+++ python/ABC/ABC100B.py
@@ -1,4 +1,15 @@
 D, N = map(int, input().split())
 
-k = N if N % 100 != 0 else N + 1   # N==100 のときだけ 101 にずらす
-print(k * (100 ** D))
+# 「100で割り切れるけど 10000では割り切れない数」だけが好きな数。
+need = 100 ** D  # 少なくともこれで割り切れる必要がある。
+need_more = 100 ** (D + 1)  # これは割り切れてはいけない。
+
+cnt = 0
+x = 1
+while True:
+    if x % need == 0 and x % need_more != 0:  # 条件を満たすか？
+        cnt += 1
+        if cnt == N:
+            print(x)
+            break
+    x += 1
