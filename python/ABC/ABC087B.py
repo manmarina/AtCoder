177a94d56467b3a768662091261e021976180afa
manmarina <manmarina@me.com>
2025-08-25 12:39:19 +0900

    087B チャッピー 枝刈り

--- python/ABC/ABC087B.py
+++ python/ABC/ABC087B.py
@@ -5,8 +5,11 @@ X = int(input())
 
 ans = 0
 for i in range(A + 1):
-    for j in range(B + 1):
-        for k in range(C + 1):
-            if 500 * i + 100 * j + 50 * k == X:
-                ans += 1
+    rem1 = X - 500 * i
+    if rem1 < 0:
+        break
+    for j in range(min(B, rem1 // 100) + 1):
+        rem2 = rem1 - 100 * j
+        if rem2 % 50 == 0 and rem2 // 50 <= C:
+            ans += 1
 print(ans)
