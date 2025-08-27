874f39179ffde37c0dba1fae287856073f4bef54
manmarina <manmarina@me.com>
2025-08-27 12:14:10 +0900

    158B チャッピー if文を使わないのでWAになりにくい

--- python/ABC/ABC158B.py
+++ python/ABC/ABC158B.py
@@ -1,14 +1,7 @@
 N, A, B = map(int, input().split())
 
-t = N // (A + B)
-blue = t * A
-rest = N - (A + B) * t
+full = N // (A + B)
+rem = N % (A + B)
+blue = full * A + min(rem, A)
 
-# print(blue)
-# print(rest)
-
-if rest > B:
-    blue += B
-else:
-    blue += rest
 print(blue)
