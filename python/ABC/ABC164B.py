d129d9a26b9978b4e4ad580c1ff0dd84d45aab91
manmarina <manmarina@me.com>
2025-08-27 16:16:10 +0900

    164B チャッピー 必要ターン数＝天井割り（ceil）

--- python/ABC/ABC164B.py
+++ python/ABC/ABC164B.py
@@ -1,16 +1,8 @@
 A, B, C, D = map(int, input().split())
 
-takahashi = True
-while 1:
-    if takahashi:
-        C -= B
-        takahashi = False
-        if C <= 0:
-            print("Yes")
-            break
-    else:
-        A -= D
-        takahashi = True
-        if A <= 0:
-            print(("No"))
-            break
+# t_T = ceil(C / B)
+t_T = (C + B - 1) // B  # 高橋→青木
+# t_A = ceil(A / D)
+t_A = (A + D - 1) // D  # 青木→高橋
+
+print("Yes" if t_T <= t_A else "No")
