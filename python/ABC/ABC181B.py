d3fc1b5fa19852a2109facbdbf8bb5de17afc60f
manmarina <manmarina@me.com>
2025-08-28 07:25:55 +0900

    181B チャッピー 等差数列の和の公式

--- python/ABC/ABC181B.py
+++ python/ABC/ABC181B.py
@@ -1,4 +1,8 @@
 N = int(input())
 AB = [list(map(int, input().split())) for _ in range(N)]
 
-print(sum(sum(range(a, b + 1)) for a, b in AB))
+ans = 0
+for a, b in AB:
+    sum = (a + b) * (b - a + 1) // 2  # 等差数列の和の公式
+    ans += sum
+print(ans)
