e78f9daf36c1a0036e157f1847842919143c2030
manmarina <manmarina@me.com>
2025-08-28 17:55:30 +0900

    209B チャッピー O(1) 総額から割引額をまとめて控除

--- python/ABC/ABC209B.py
+++ python/ABC/ABC209B.py
@@ -1,10 +1,5 @@
 N, X = map(int, input().split())
 A = list(map(int, input().split()))
 
-cost = 0
-for i, a in enumerate(A, start=1):  # 1始まりに注意
-    if i % 2 == 0:
-        cost += a - 1
-    else:
-        cost += a
-print("Yes" if cost <= X else "No")
+total = sum(A) - (N // 2)   # 偶数番目の個数だけ1円引き
+print("Yes" if total <= X else "No")
