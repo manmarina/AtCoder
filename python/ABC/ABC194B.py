16dce07a38a0f8a00477e421ce27e0d48a9ceabc
manmarina <manmarina@me.com>
2025-08-28 12:20:56 +0900

    194B チャッピー 理解できず

--- python/ABC/ABC194B.py
+++ python/ABC/ABC194B.py
@@ -8,21 +8,21 @@ for _ in range(N):
     A.append(a)
     B.append(b)
 
-# print(A)
-# print(B)
+print(A)
+print(B)
 
 idx_A, min_A = min(enumerate(A), key=lambda x: x[1])
 idx_B, min_B = min(enumerate(B), key=lambda x: x[1])
 
-# print(idx_A, min_A)
-# print(idx_B, min_B)
+print(idx_A, min_A)
+print(idx_B, min_B)
 
 if idx_A == idx_B:
     min_A2 = min((a for i, a in enumerate(A) if i != idx_A), default=math.inf)
     min_B2 = min((a for i, a in enumerate(B) if i != idx_B), default=math.inf)
 
-    # print(min_A2)
-    # print(min_B2)
+    print(min_A2)
+    print(min_B2)
 
     print(min(min_A2, min_B2, min_A + min_B))
 else:
