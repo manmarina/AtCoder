05ef4383cb063c251a10060986611676bf506d94
manmarina <manmarina@me.com>
2025-08-29 13:06:06 +0900

    237B チャッピー zipを用いて簡単に転置

--- python/ABC/ABC237B.py
+++ python/ABC/ABC237B.py
@@ -1,10 +1,8 @@
 H, W = map(int, input().split())
 A = [list(map(int, input().split())) for _ in range(H)]
 
-AT = [[0] * H for _ in range(W)]
-for i in range(H):
-    for j in range(W):
-        AT[j][i] = A[i][j]
+# 転置
+AT = list(zip(*A))   # 各要素はタプルになる
 
 for row in AT:
     print(*row)
