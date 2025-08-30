ce28607251978e3b95df7c7e8ddcf724d509101a
manmarina <manmarina@me.com>
2025-08-31 07:24:23 +0900

    271B 2回目

--- python/ABC/ABC271B.py
+++ python/ABC/ABC271B.py
@@ -1,13 +1,9 @@
 N, Q = map(int, input().split())
+L = [list(map(int, input().split())) for _ in range(N)]
+S = [list(map(int, input().split())) for _ in range(Q)]
 
-a = []
-for i in range(N):
-    a.append(list(map(int, input().split())))
+# print(L)
+# print(S)
 
-answer = []
-for i in range(Q):
-    s, t = map(int, input().split())
-    answer.append(a[s - 1][t])
-
-for i in range(Q):
-    print(answer[i])
+for sq, tq in S:
+    print(L[sq - 1][tq])
