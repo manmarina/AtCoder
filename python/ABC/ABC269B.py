4a9aecb768a44bf14fe25397f824ec6944eba670
manmarina <manmarina@me.com>
2025-08-31 00:20:19 +0900

    269B 2回目

--- python/ABC/ABC269B.py
+++ python/ABC/ABC269B.py
@@ -1,32 +1,27 @@
-S = []
-for i in range(10):
-    s = input()
-    S.append(s)
+S = [input() for _ in range(10)]
+# print(S)
 
-D = 10
+# 左上
 flag = False
 for i in range(10):
     for j in range(10):
-        if S[i][j] == '#' and not flag:
+        if S[i][j] == '#':
             A = i + 1
             C = j + 1
             flag = True
-        if S[i][j] == '.' and flag:
-            D = j
             break
     if flag:
         break
 
-B = 10
+# 右下
 flag = False
-for i in range(A, 10):
-    for j in range(10):
+for i in range(9, -1, -1):
+    for j in range(9, -1, -1):
         if S[i][j] == '#':
+            B = i + 1
+            D = j + 1
+            flag = True
             break
-    else:
-        B = i
-        flag = True
-        break
     if flag:
         break
 
