89db4c6eebd82202d30ac1edabd5ff6924415996
manmarina <manmarina@me.com>
2025-08-31 07:13:28 +0900

    270B チャッピー 4P4の半分=12通りに整理

--- python/ABC/ABC270B.py
+++ python/ABC/ABC270B.py
@@ -1,8 +1,12 @@
 X, Y, Z = map(int, input().split())
 
-if 0 < Y < X < Z or 0 < Y < Z < X or X < Z < Y < 0 or Z < X < Y < 0:
-    print(-1)
-elif X < Y < 0 < Z or Z < 0 < Y < X:
-    print(2 * abs(Z) + abs(X))
-else:
+# 座標4つの並びは4P4だが、左右対処なので、Y<0に固定すると4P4の半分で12通り
+if Y < 0:
+    X, Y, Z = -X, -Y, -Z
+
+if X < Y:  # ゴールが壁の手前
     print(abs(X))
+elif Y < Z:  # ハンマーが壁の奥
+    print(-1)
+else:  # ゴールと反対方向にハンマーを拾いに行ってからゴール
+    print(abs(Z) + abs(X - Z))
