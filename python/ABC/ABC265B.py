d9d24ee53fa82c9e4b051f81f540f18d871f1217
manmarina <manmarina@me.com>
2025-08-30 20:37:55 +0900

    265B 2回目 TLE

--- python/ABC/ABC265B.py
+++ python/ABC/ABC265B.py
@@ -1,22 +1,21 @@
 N, M, T = map(int, input().split())
-A = [0] + list(map(int, input().split()))
+A = list(map(int, input().split()))
+XY = [list(map(int, input().split())) for _ in range(M)]
 
-bonus = [0] * (N + 1)
-for i in range(M):
-    X, Y = map(int, input().split())
-    bonus[X] = Y
+# print(XY)
 
-# print(A)
-# print(bonus)
-
-for i in range(1, N):
-    T += bonus[i]
-    T -= A[i]
-
-    if T <= 0:
-        print("No")
-        # print(T)
-        break
-else:
-    print("Yes")
-    # print(T)
+time = T
+i = 1
+while time > 0:
+    for a in A:
+        time -= a
+        if time < 0:
+            print("No")
+            exit()
+        i += 1
+        for x, y in XY:
+            if i == x:
+                time += y
+        if i == N:
+            print("Yes")
+            exit()
