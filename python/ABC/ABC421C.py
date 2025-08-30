8e1479e8e3a3d394d673ea47e0e72a08bb7d38af
manmarina <manmarina@me.com>
2025-08-30 22:49:46 +0900

    421C 1パスに変更するがTLE

--- python/ABC/ABC421C.py
+++ python/ABC/ABC421C.py
@@ -2,42 +2,37 @@ N = int(input())
 S = [*input()]
 
 # print(S)
-
-if S[0] == 'A':
-    ok = 'AB' * N
-else:
-    ok = 'BA' * N
-ok = [*ok]
-
 # print(ok)
 
 count = 0
-while 1:
-    for i in range(N * 2 - 2):
-        if S[i] == 'A' and S[i + 1] == 'A' and S[i + 2] == 'B':
-            S[i + 1], S[i + 2] = S[i + 2], S[i + 1]
-            count += 1
+for i in range(N * 2 - 2):
+    if S[i] == 'A':
+        dist = 0
+        for j in range(i + 1, N * 2):
+            if S[j] == 'B':
+                break
+            dist += 1
 
-            # print(S)
+        # print(dist)
 
-            if S == ok:
+        count += dist
+        S[i + 1] = 'B'
+        if dist != 0:
+            S[i + dist + 1] = 'A'
 
-                # print(S)
+    else:  # S[i] == 'B'
+        dist = 0
+        for j in range(i + 1, N * 2):
+            if S[j] == 'A':
+                break
+            dist += 1
 
-                print(count)
-                exit()
-            break
+        # print(dist)
 
-        if S[i] == 'B' and S[i + 1] == 'B' and S[i + 2] == 'A':
-            S[i + 1], S[i + 2] = S[i + 2], S[i + 1]
-            count += 1
+        count += dist
+        S[i + 1] = 'A'
+        if dist != 0:
+            S[i + dist + 1] = 'B'
 
-            # print(S)
-
-            if S == ok:
-
-                # print(S)
-
-                print(count)
-                exit()
-            break
+# print(S)
+print(count)
