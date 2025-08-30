7b32399d46edf60e9918fee28e575eada7283198
manmarina <manmarina@me.com>
2025-08-30 13:02:07 +0900

    261B 2回目

--- python/ABC/ABC261B.py
+++ python/ABC/ABC261B.py
@@ -1,21 +1,16 @@
-N = int(input())
-
-A = []
-for i in range(N):
-    A.append(input())
+N = int(input().strip())
+A = [input().strip() for _ in range(N)]
 
 for i in range(N):
-    for j in range(N):
-        if i == j:
-            continue
-        if A[i][j] == 'W' and A[j][i] != 'L':
+    for j in range(i + 1, N):
+        if A[i][j] == 'D' and A[j][i] != 'D':
+            print("incorrect")
+            exit()
+        elif A[i][j] == 'W' and A[j][i] != 'L':
             print("incorrect")
             exit()
         elif A[i][j] == 'L' and A[j][i] != 'W':
             print("incorrect")
             exit()
-        elif A[i][j] == 'D' and A[j][i] != 'D':
-            print("incorrect")
-            exit()
-else:
-    print("correct")
+
+print("correct")
