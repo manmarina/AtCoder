8eff35016003d1157aa77b67f028b3d3bae7378b
manmarina <manmarina@me.com>
2025-08-30 12:30:07 +0900

    260B クラスを生徒ごとに作成

--- python/ABC/ABC260B.py
+++ python/ABC/ABC260B.py
@@ -1,50 +1,55 @@
-class Entry:
-    def __init__(self, score, id):
-        self.score = score
+class Student:
+    def __init__(self, id, math, eng):
         self.id = id
+        self.math = math
+        self.eng = eng
+        self.both = math + eng
 
-    def sort_key(self):
-        # 得点降順・同点はid昇順
-        return (-self.score, self.id)
+    # def __repr__(self):
+    # return f"(id={self.id}, math={self.math}, eng={self.eng},
+    # both={self.both})"
 
 
 N, X, Y, Z = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-math = [Entry(A[i], i + 1) for i in range(N)]
-eng = [Entry(B[i], i + 1) for i in range(N)]
-both = [Entry(A[i] + B[i], i + 1) for i in range(N)]
-
-math.sort(key=lambda e: e.sort_key())
-eng.sort(key=lambda e: e.sort_key())
-both.sort(key=lambda e: e.sort_key())
+students = [Student(i + 1, A[i], B[i]) for i in range(N)]
 
 ans = []
 
 cnt = 0
-for e in math:
+for s in sorted(students, key=lambda x: (-x.math, x.id)):
     if cnt == X:
         break
-    if e.id not in ans:
-        ans.append(e.id)
+    if s.id not in ans:
+        ans.append(s.id)
         cnt += 1
 
+# print(sorted(students, key=lambda x: (-x.math, x.id)))
+# print(ans)
+
 cnt = 0
-for e in eng:
+for s in sorted(students, key=lambda x: (-x.eng, x.id)):
     if cnt == Y:
         break
-    if e.id not in ans:
-        ans.append(e.id)
+    if s.id not in ans:
+        ans.append(s.id)
         cnt += 1
 
+# print(sorted(students, key=lambda x: (-x.eng, x.id)))
+# print(ans)
+
 cnt = 0
-for e in both:
+for s in sorted(students, key=lambda x: (-x.both, x.id)):
     if cnt == Z:
         break
-    if e.id not in ans:
-        ans.append(e.id)
+    if s.id not in ans:
+        ans.append(s.id)
         cnt += 1
+
+# print(sorted(students, key=lambda x: (-x.both, x.id)))
+# print(ans)
 
 ans.sort()
 print(*ans, sep='\n')
