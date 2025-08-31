18121d135d64405f428a02a0a65cc17018e5d290
manmarina <manmarina@me.com>
2025-09-01 08:04:48 +0900

    283B 2回目

--- python/ABC/ABC283B.py
+++ python/ABC/ABC283B.py
@@ -1,13 +1,13 @@
 N = int(input())
-A = list(map(int, input().split()))
-
-query = []
+A = [0] + list(map(int, input().split()))
 Q = int(input())
-for i in range(Q):
-    query.append(list(map(int, input().split())))
+ql = [list(map(int, input().split())) for _ in range(Q)]
 
-for i in range(Q):
-    if query[i][0] == 1:
-        A[query[i][1] - 1] = query[i][2]
-    else:
-        print(A[query[i][1] - 1])
+# print(A)
+# print(ql)
+
+for q in ql:
+    if q[0] == 1:
+        A[q[1]] = q[2]
+    else:  # q[0] == 2
+        print(A[q[1]])
