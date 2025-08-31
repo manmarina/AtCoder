5740bd62a4d67f3b5f57bb6ed45addd03bbae0c7
manmarina <manmarina@me.com>
2025-09-01 07:49:20 +0900

    282B 初回の回答 ビットフラグを使ったスマートな解法で驚いた

--- python/ABC/ABC282B.py
+++ python/ABC/ABC282B.py
@@ -1,13 +1,29 @@
-from itertools import combinations
-
 N, M = map(int, input().split())
-S = [input() for _ in range(N)]
+
+S = []
+point = []
+for i in range(N):
+    S.append(input())
+    point.append(0)
+
+# print(S)
+# print(point)
+
+# 点数表を作成
+for i in range(N):
+    for j in range(M):
+        if S[i][j] == 'o':
+            point[i] += 2**j  # 各人の点数をビットフラグで格納
+
+# print(point)
+
 
 count = 0
-for s1, s2 in combinations(S, 2):
-    for c1, c2 in zip(s1, s2):
-        if c1 == 'x' and c2 == 'x':
-            break
-    else:
-        count += 1
+for i in range(N):
+    for j in range(i + 1, N):
+        if point[i] | point[j] == 2**M - 1:  # ペアの点数のorが11111ならcount++
+            count += 1
+
+# print(point)
+
 print(count)
