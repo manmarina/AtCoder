76fab11bb1f4e8b9ea931de26ea1e9dc201625f5
manmarina <manmarina@me.com>
2025-09-02 12:10:38 +0900

    296B maketrans/translateを用いた解

--- python/ABC/ABC296B.py
+++ python/ABC/ABC296B.py
@@ -1,11 +1,14 @@
 S = [input() for _ in range(8)]
 
 # print(S)
+# l = str.maketrans({'0': 'a', '1': 'b', '2': 'c', '3': 'd', '4': 'e', '5': 'f', '6': 'g', '7': 'h'})
+l = str.maketrans('01234567', 'abcdefgh')
+r = str.maketrans('76543210', '12345678')
 
 for i in range(8):
     for j in range(8):
         if S[i][j] == '*':
-            r = str(8 - i)
-            c = chr(ord('a') + j)
+            c = str(j).translate(l)
+            r = str(i).translate(r)
             print(c + r)
             exit()
