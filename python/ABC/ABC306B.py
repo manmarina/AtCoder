3cdf6c15f787dc8f22d30c56bb7133a711553481
manmarina <manmarina@me.com>
2025-09-02 19:23:58 +0900

    306B チャッピー ビットシフトによる解

--- python/ABC/ABC306B.py
+++ python/ABC/ABC306B.py
@@ -1,3 +1,6 @@
 A = list(map(int, input().split()))
-
-print(int(''.join(map(str, A[::-1])), 2))
+ans = 0
+for i, a in enumerate(A):   # A[0] が 2^0、A[63] が 2^63
+    if a:
+        ans += 1 << i
+print(ans)
