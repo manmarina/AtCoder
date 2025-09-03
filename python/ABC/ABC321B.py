9c7b73bc82986291345193d97b148c5b4d865e77
manmarina <manmarina@me.com>
2025-09-03 16:52:12 +0900

    321B 全探索によりifは合格判定の1回のみ

--- python/ABC/ABC321B.py
+++ python/ABC/ABC321B.py
@@ -1,27 +1,12 @@
 N, X = map(int, input().split())
 A = list(map(int, input().split()))
 
-A.sort()
-sm = sum(A[1:-1])
-
-# print(A)
-# print(sm)
-
-if N == 3:
-    if A[0] >= X:
-        print(0)
-        exit()
-    elif A[1] >= X:
-        print(A[1])
-        exit()
-    else:
-        print(-1)
-        exit()
-
-need = X - sm
-if need <= 0:
-    print(0)
-elif need <= 100:
-    print(need)
+for i in range(100 + 1):
+    temp = A.copy()
+    temp.append(i)
+    sm = sum(temp) - max(temp) - min(temp)
+    if sm >= X:
+        print(i)
+        break
 else:
     print(-1)
