d5b0396f2f45e9ff60b44e5b7be006373258501c
manmarina <manmarina@me.com>
2025-09-04 08:41:36 +0900

    326B チャッピー 各桁を計算で求める

--- python/ABC/ABC326B.py
+++ python/ABC/ABC326B.py
@@ -1,8 +1,9 @@
 N = int(input())
 
-for i in range(N, 919 + 1):
-    t = str(i)
-    a, b, c = int(t[0]), int(t[1]), int(t[2])
-    if c == a * b:
-        print(i)
+for n in range(N, 920):  # 919 を含める
+    a = n // 100
+    b = (n // 10) % 10
+    c = n % 10
+    if a * b == c:
+        print(n)
         break
