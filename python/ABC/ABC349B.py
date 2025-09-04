bbcb9906406aae2e189d2c8e3d65d2b3eb8e9d3d
manmarina <manmarina@me.com>
2025-09-05 08:52:58 +0900

    349B チャッピー

--- python/ABC/ABC349B.py
+++ python/ABC/ABC349B.py
@@ -1,12 +1,14 @@
-from collections import Counter
-
 S = input()
 
-cnt = Counter(S)
-for i in range(1, -(-len(S) // 2)):
-    kinds = len([c for c in cnt.values() if c == i])
-    if kinds != 2 and kinds != 0:
-        print("No")
-        break
-else:
-    print("Yes")
+cnt = [0] * 26
+for c in S:
+    cnt[ord(c) - ord('a')] += 1
+print(cnt)
+
+cnt2 = [0] * (len(S) + 1)
+for x in cnt:
+    if x > 0:
+        cnt2[x] += 1
+print(cnt2)
+
+print("Yes" if all(v in (0, 2) for v in cnt2) else "No")
