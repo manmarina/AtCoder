496a839650fb29f5345a38085839c1f3c798e18e
manmarina <manmarina@me.com>
2025-09-04 09:53:10 +0900

    328B set(str())でゾロ目判定を簡略化

--- python/ABC/ABC328B.py
+++ python/ABC/ABC328B.py
@@ -4,16 +4,8 @@ D = list(map(int, input().split()))
 count = 0
 for m in range(1, N + 1):
     for d in range(1, D[m - 1] + 1):
-        judge = set()
-        m1, m2 = m, m
-        d1, d2 = d, d
-        if m >= 10:
-            m1 = m // 10
-            m2 = m % 10
-        if d >= 10:
-            d1 = d // 10
-            d2 = d % 10
-        judge.update([m1, m2, d1, d2])
-        if len(judge) == 1:
+        ms = set(str(m))
+        ds = set(str(d))
+        if len(ms) == 1 and ms == ds:
             count += 1
 print(count)
