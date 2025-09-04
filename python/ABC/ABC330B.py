dde744b93648fac8eede4bbaf295d451ea69cd97
manmarina <manmarina@me.com>
2025-09-04 10:48:24 +0900

    330B チャッピー ワンライナー

--- python/ABC/ABC330B.py
+++ python/ABC/ABC330B.py
@@ -1,13 +1,5 @@
 N, L, R = map(int, input().split())
 A = list(map(int, input().split()))
 
-ans = []
-for a in A:
-    # L以下ならL　R以上ならR　中間ならそのまま
-    if a < L:
-        ans.append(L)
-    elif a > R:
-        ans.append(R)
-    else:
-        ans.append(a)
-print(*ans)
+X = [min(max(a, L), R) for a in A]
+print(*X)
