42f09eb16dbbd233b30d0de2d0797445248770ef
manmarina <manmarina@me.com>
2025-09-04 15:28:08 +0900

    336B チャッピー ビット演算

--- python/ABC/ABC336B.py
+++ python/ABC/ABC336B.py
@@ -1,4 +1,4 @@
 N = int(input())
 
-b = bin(N)               # '0b...'
-print(len(b) - len(b.rstrip('0')))
+lowbit = N & -N          # 2^k N&-Nは最下位ビットのみが立つ性質がある
+print(lowbit.bit_length() - 1)  # 最下位ビットの長さ−1が末尾の0の長さ
