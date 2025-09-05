616cadc1c9c11b3626900af73a40d04da09774d5
manmarina <manmarina@me.com>
2025-09-06 07:53:41 +0900

    372B チャッピー

--- python/ABC/ABC372B.py
+++ python/ABC/ABC372B.py
@@ -1,12 +1,20 @@
-from math import log
-
 M = int(input())
 
-m = M
 ans = []
-while m > 0:
-    p = int(log(m, 3))
-    ans.append(p)
-    m = m - 3**p
+i = 0
+while M > 0:
+    cnt = M % 3
+    ans.extend([i] * cnt)
+    M //= 3
+    i += 1
+
 print(len(ans))
 print(*ans)
+
+"""
+10進数で 123 を「1の位・10の位・100の位」に分けるときも、
+123 % 10 = 3 が 1の位
+123 // 10 = 12 にして繰り返す
+とやっています。
+これと同じことを 3進法 でやるだけです。
+"""
