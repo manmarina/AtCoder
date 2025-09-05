3a1485284ee604a31c86d07958e1eaf536f12833
manmarina <manmarina@me.com>
2025-09-05 09:11:28 +0900

    349B チャッピーに教えてもらってWA解消

--- python/ABC/ABC349B.py
+++ python/ABC/ABC349B.py
@@ -1,14 +1,13 @@
+from collections import Counter
+
 S = input()
 
-cnt = [0] * 26
-for c in S:
-    cnt[ord(c) - ord('a')] += 1
-print(cnt)
+cnt = Counter(S)
+# print(cnt)
 
-cnt2 = [0] * (len(S) + 1)
-for x in cnt:
-    if x > 0:
-        cnt2[x] += 1
-print(cnt2)
+ans = []
+for i in range(len(S) + 1):  # len(S) "+1"　が重要 例：S="aaaaa"
+    ans.append(len([c for c in cnt.values() if c == i]))
+# print(ans)
 
-print("Yes" if all(v in (0, 2) for v in cnt2) else "No")
+print("Yes" if all(a in (0, 2) for a in ans) else "No")
