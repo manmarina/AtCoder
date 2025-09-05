5fcab454ad9d08a427c6d87803590ea2cd00ec8e
manmarina <manmarina@me.com>
2025-09-06 08:33:18 +0900

    373B チャッピー キーの位置を格納する配列を作成

--- python/ABC/ABC373B.py
+++ python/ABC/ABC373B.py
@@ -1,14 +1,10 @@
-S = input()
+S = input().strip()           # 長さ26、A..Zが一度ずつ
+
+pos = [0] * 26
+for i, ch in enumerate(S):
+    pos[ord(ch) - 65] = i     # 'A' のASCIIは 65
 
 ans = 0
-for i in range(65, 90):
-    cur = chr(i)
-    nex = chr(i + 1)
-    # print(cur, nex)
-
-    icur = S.find(cur)
-    inex = S.find(nex)
-    # print(icur, inex)
-
-    ans += abs(inex - icur)
+for x in range(25):           # A→B, ..., Y→Z の25回
+    ans += abs(pos[x + 1] - pos[x])
 print(ans)
