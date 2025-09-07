ca1ebe18a4db39e524f862ff246843c41dad3969
manmarina <manmarina@me.com>
2025-09-07 15:07:27 +0900

    422C チャッピーのロジックを採用してAC

--- python/ABC/ABC422C.py
+++ python/ABC/ABC422C.py
@@ -1,27 +1,7 @@
-import sys
-
-input = sys.stdin.buffer.readline
 T = int(input())
+tc = [list(map(int, input().split())) for _ in range(T)]
+# print(tc)
 
-out = []
-for _ in range(T):
-    a, b, c = map(int, input().split())
-    total = a + b + c
-    out.append(str(min(a, c, total // 3)))
-print("\n".join(out))
-
-
-"""
-bが最小で、a==cの時
-101 -> 0
-202 -> 1
-303 -> 2
-404 -> 2
-505 -> 3
-606 -> 4
-707 -> 4
-808 -> 5
-909 -> 6
-以上正しく動作している。
-自分のコードではこの通りの回答が得られなかった。
-"""
+for na, nb, nc in tc:
+    total = na + nb + nc
+    print(min(na, nc, total // 3))
