11ab05132654d8d92a1bca84d62da3c28eafd88f
manmarina <manmarina@me.com>
2025-09-08 08:43:43 +0900

    399B初回

---
+++ python/ABC/ABC399B.py
@@ -0,0 +1,27 @@
+N = int(input())
+P = list(map(int, input().split()))
+
+P = [(i, p) for i, p in enumerate(P, 1)]
+P.sort(key=lambda x: (-x[1], x[0]))
+# print(P)
+
+
+rank = 1
+count = 0
+ans = [(P[0][0], 1)]
+pre = P[0][1]
+for i in range(1, N):
+    if P[i][1] == pre:
+        count += 1
+        ans.append((P[i][0], rank))
+    else:
+        rank = rank + 1 + count
+        count = 0
+        ans.append((P[i][0], rank))
+        pre = P[i][1]
+# print(ans)
+
+ans.sort(key=lambda x: x[0])
+# print(ans)
+for i in range(N):
+    print(ans[i][1])
