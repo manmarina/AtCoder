fa6e03241086a84b2712aeacdc1126d19a749a97
manmarina <manmarina@me.com>
2025-09-07 14:49:30 +0900

    422C WA直らず bが最小でaとcが同じ時の答えが正答できないことまでわかった

--- python/ABC/ABC422C.py
+++ python/ABC/ABC422C.py
@@ -3,9 +3,11 @@ tc = [list(map(int, input().split())) for _ in range(T)]
 # print(tc)
 
 for na, nb, nc in tc:
-    d = abs(na - nc)
-    mn = min(na, nb, nc)
-    ma = na - mn
-    mc = nc - mn
-    p = min(min(ma, mc), max(ma, mc) // 2)
-    print(mn + p)
+    if min(na, nb, nc) != nb:
+        print(min(na, nc))
+    else:
+        ans = nb
+        na -= nb
+        nc -= nb
+        ans += min(min(na, nc), max(na, nc) // 2)
+        print(ans)
