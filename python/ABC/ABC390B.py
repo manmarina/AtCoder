d16addbde65a0a4d0d53e154a4b12c24516b580a
manmarina <manmarina@me.com>
2025-09-07 09:27:49 +0900

    390B isclose()で判定してみたがWA

--- python/ABC/ABC390B.py
+++ python/ABC/ABC390B.py
@@ -1,10 +1,12 @@
+from math import isclose
+
 N = int(input())
 A = list(map(int, input().split()))
 # print(A)
 
 r = A[1] / A[0]
 for i in range(1, N - 1):
-    if A[i + 1] / A[i] != r:
+    if not isclose(A[i + 1] / A[i], r):
         print("No")
         break
 else:
