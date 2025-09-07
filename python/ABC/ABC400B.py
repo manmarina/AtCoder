2401a426f912eb38ebd07076034035742f5ba6ca
manmarina <manmarina@me.com>
2025-09-08 06:54:38 +0900

    400B チャッピー オーバーフローしないように判定しながら進める

--- python/ABC/ABC400B.py
+++ python/ABC/ABC400B.py
@@ -1,11 +1,23 @@
 N, M = map(int, input().split())
+LIM = 10**9
 
-INF = 10**9
-X = 0
+if N == 1:
+    ans = M + 1
+    print(ans if ans <= LIM else "inf")
+    exit()
+
+s = 0  # s=すべての項の合計
+t = 1  # N^0　t=今の項の値(N^i)
 for i in range(M + 1):
-    X += N**i
-    if X > INF:
+    s += t
+    if s > LIM:
         print("inf")
         break
+    # 次の項 t *= N を作る前に安全判定
+    if i < M:
+        if t > LIM // N:  # t * N > LIM なら危険なので事前に判定
+            print("inf")
+            break
+        t *= N
 else:
-    print(X)
+    print(s)
