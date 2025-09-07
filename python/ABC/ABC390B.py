014520eb21213815efafdf45c38f132ac3459560
manmarina <manmarina@me.com>
2025-09-07 09:38:48 +0900

    390B チャッピー クロス積を三項の積で判定

--- python/ABC/ABC390B.py
+++ python/ABC/ABC390B.py
@@ -1,11 +1,8 @@
 N = int(input())
 A = list(map(int, input().split()))
-# print(A)
 
-a0, a1 = A[0], A[1]
-for i in range(1, N - 1):
-    if A[i + 1] * a0 != A[i] * a1:  # クロス積が等しければ公比数列
-        print("No")
-        break
-else:
+if N <= 2:
     print("Yes")
+else:
+    ok = all(A[i] * A[i + 2] == A[i + 1] * A[i + 1] for i in range(N - 2))
+    print("Yes" if ok else "No")
