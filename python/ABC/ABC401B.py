1d195b824be8248a8a5a512e124551d8ade0c143
manmarina <manmarina@me.com>
2025-09-08 10:30:06 +0900

    401B チャッピー 同じロジック

--- python/ABC/ABC401B.py
+++ python/ABC/ABC401B.py
@@ -1,13 +1,14 @@
 N = int(input())
-S = [input() for _ in range(N)]
 
-count = 0
-islogin = False
-for s in S:
-    if s == "private" and not islogin:
-        count += 1
-    elif s == "login":
-        islogin = True
+logged = False
+ans = 0
+for _ in range(N):
+    s = input().strip()
+    if s == "login":
+        logged = True
     elif s == "logout":
-        islogin = False
-print(count)
+        logged = False
+    elif s == "private":
+        if not logged:
+            ans += 1
+print(ans)
