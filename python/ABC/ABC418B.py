73eda625aa6c9a9027f4cf6e6961fbb7fe1f4bbb
manmarina <manmarina@me.com>
2025-09-09 08:55:20 +0900

    428B チャッピー 累積和配列を使用して高速化

--- python/ABC/ABC418B.py
+++ python/ABC/ABC418B.py
@@ -1,19 +1,30 @@
-S = input()
+S = input().strip()
+n = len(S)
 
-ls = len(S)
-ans = 0
-for i in range(ls - 2):
-    # print(i)
-    for j in range(i + 2, ls):
-        # print(j, end=" ")
-        # print(S[i:j + 1])
-        s = S[i:j + 1]
-        if s[0] == 't' and s[-1] == 't':
-            ct = s.count('t')
-            rate = (ct - 2) / (len(s) - 2)
+# 累積和: pref[k] = S[0:k] に含まれる 't' の個数
+pref = [0] * (n + 1)
+for i, ch in enumerate(S, 1):
+    pref[i] = pref[i - 1] + (ch == 't')
+print(pref)
 
-            # print(s)
-            # print(rate)
 
-            ans = max(ans, rate)
-print(ans)
+best = 0.0
+
+# 早期終了: 'ttt' があれば 1
+if 'ttt' in S:
+    print("1.0")
+    exit()
+
+for i in range(n):
+    if S[i] != 't':
+        continue
+    for j in range(i + 2, n):  # 長さ>=3
+        if S[j] != 't':
+            continue
+        num_t_inside = pref[j] - pref[i + 1]
+        len_inside = j - i - 1
+        rate = num_t_inside / len_inside  # 0 <= rate <= 1
+        if rate > best:
+            best = rate
+
+print("{:.17f}".format(best))
