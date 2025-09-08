9f54c1da865ee6eb0465da7ef36c6e44b4a20cc0
manmarina <manmarina@me.com>
2025-09-08 21:26:43 +0900

    411B チャッピー 同じロジック

--- python/ABC/ABC411B.py
+++ python/ABC/ABC411B.py
@@ -1,10 +1,10 @@
 N = int(input())
 D = list(map(int, input().split()))
 
-for i in range(N - 1):
-    temp = []
-    cs = 0
-    for j in range(i, N - 1):
-        cs += D[j]
-        temp.append(cs)
-    print(*temp)
+for i in range(N - 1):        # 0..N-2 が駅 i+1 を表す
+    cur = 0
+    row = []
+    for j in range(i, N - 1):  # 区間 D_i..D_{j} を累積
+        cur += D[j]
+        row.append(str(cur))
+    print(" ".join(row))
