38b0fb0ac444edaeb0e1333a2d95272b5a6ba11e
manmarina <manmarina@me.com>
2025-09-10 15:30:14 +0900

    045C 理解しやすくするため、ansを配列にして初回と同様の構造に変更

--- python/ABC/ABC045C.py
+++ python/ABC/ABC045C.py
@@ -1,13 +1,16 @@
 S = input().strip()
 n = len(S)
-ans = 0
 
+ans = []
 for mask in range(1 << (n - 1)):  # ビットマスクを生成 2**(n-1)
     cur = 0
+    temp = 0
     for i, ch in enumerate(S):
         cur = cur * 10 + int(ch)  # 数字を伸ばす
         if i == n - 1 or (mask >> i) & 1:  # 文字列の最後の文字か、ビットマスクが1の場所なら
-            ans += cur
+            temp += cur
             cur = 0
+    ans.append(temp)
 
-print(ans)
+# print(ans)
+print(sum(ans))
