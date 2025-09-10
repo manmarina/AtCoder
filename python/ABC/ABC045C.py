0cd0d238110443d35555e35d977b4e02dc58a87e
manmarina <manmarina@me.com>
2025-09-10 15:21:38 +0900

    045C チャッピー ビット全探索

--- python/ABC/ABC045C.py
+++ python/ABC/ABC045C.py
@@ -1,30 +1,13 @@
-from itertools import product
+S = input().strip()
+n = len(S)
+ans = 0
 
-S = input()
+for mask in range(1 << (n - 1)):  # ビットマスクを生成 2**(n-1)
+    cur = 0
+    for i, ch in enumerate(S):
+        cur = cur * 10 + int(ch)  # 数字を伸ばす
+        if i == n - 1 or (mask >> i) & 1:  # 文字列の最後の文字か、ビットマスクが1の場所なら
+            ans += cur
+            cur = 0
 
-# +の位置を決定
-bit = [0, 1]
-bits = []
-for b in product(bit, repeat=len(S) - 1):
-    bits.append(b)
-# print(bits)
-
-# +を組み込んだ数式を配列に格納
-formulas = []
-for bs in bits:
-    formula = [S[0]]
-    for i in range(1, len(S)):
-        if bs[i - 1] == 1:
-            formula.append('+')
-        formula.append(S[i])
-    formulas.append(''.join(formula))
-# print(formulas)
-
-# 配列内の数式の計算結果を合計
-ans = []
-for f in formulas:
-    ans.append(eval(f, {"__builtins__": None}, {}))
-# print(ans)
-
-# 結果出力
-print(sum(ans))
+print(ans)
