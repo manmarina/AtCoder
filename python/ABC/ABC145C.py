b4096f0e20ca2233b778223680030873b41ac911
manmarina <manmarina@me.com>
2025-09-11 16:38:07 +0900

    145C チャッピー 距離行列から距離を求める hypot演算の少ない賢い実装

--- python/ABC/ABC145C.py
+++ python/ABC/ABC145C.py
@@ -1,18 +1,45 @@
+import math
 from itertools import permutations
-from math import hypot
-from statistics import mean
-
 
 N = int(input())
-xy = [list(map(int, input().split()))for _ in range(N)]
-# print(xy)
+P = [tuple(map(int, input().split())) for _ in range(N)]
 
-ans = []
-for t in permutations(xy, N):
-    temp = []
-    for i in range(1, N):
-        temp.append(hypot(t[i - 1][0] - t[i][0], t[i - 1][1] - t[i][1]))
-    ans.append(sum(temp))
+# 距離行列を作成
+dist = [[0.0] * N for _ in range(N)]
+for i in range(N):
+    xi, yi = P[i]
+    for j in range(N):
+        xj, yj = P[j]
+        dist[i][j] = math.hypot(xi - xj, yi - yj)
+# print(dist)
 
-# print(ans)
-print(mean(ans))
+# 距離行列から距離を求める
+total = 0.0
+cnt = 0
+for order in permutations(range(N)):
+    # print(order) ex.(0,1,2)
+
+    path = 0.0
+    for a, b in zip(order, order[1:]):  # ex.(0,1),(1,2)
+        path += dist[a][b]
+    total += path
+    cnt += 1
+
+print(total / cnt)
+
+"""
+毎回距離を計算すると重いので、最初に距離行列 dist[i][j] を作ると速く・きれい。
+「距離行列」というのは、全ての点のペアごとの距離を前もって計算して表にしておく仕組みです。
+
+これを二次元配列（行列）にするとこうなります：
+	P0	P1	P2
+P0	0	1	2
+P1	1	0	√5
+P2	2	√5	0
+
+たとえば巡回順序が (P0 → P1 → P2) なら、
+合計距離は
+dist[0][1] + dist[1][2] = 1 + √5
+もし (P2 → P0 → P1) なら、
+dist[2][0] + dist[0][1] = 2 + 1
+"""
