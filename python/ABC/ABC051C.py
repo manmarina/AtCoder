3b0fe7bf48c674668a9aa8439ebd865ae4f075c8
manmarina <manmarina@me.com>
2025-09-12 14:24:58 +0900

    051C チャッピー ほぼ同じロジック

--- python/ABC/ABC051C.py
+++ python/ABC/ABC051C.py
@@ -1,10 +1,11 @@
 sx, sy, tx, ty = map(int, input().split())
+dx, dy = tx - sx, ty - sy
 
-dx = tx - sx
-dy = ty - sy
+ans = []
+# 1往復（まっすぐ）
+ans.append("R" * dx + "U" * dy + "L" * dx + "D" * dy)
+# 外周ループ（1マスふくらませる）
+ans.append("D" + "R" * (dx + 1) + "U" * (dy + 1) + "L")
+ans.append("U" + "L" * (dx + 1) + "D" * (dy + 1) + "R")
 
-first = 'R' * dx + 'U' * dy + 'L' * dx + 'D' * dy
-second = 'D' + 'R' * (dx + 1) + 'U' * (dy + 1) + 'LU' + \
-    'L' * (dx + 1) + 'D' * (dy + 1) + 'R'
-
-print(first + second)
+print("".join(ans))
