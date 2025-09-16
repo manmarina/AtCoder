b7de1680a08d5f7ac73e25ed5b85e5a0da70c37f
manmarina <manmarina@me.com>
2025-09-16 19:03:58 +0900

    305B初回 スライス和を使った解

--- python/ABC/ABC305B.py
+++ python/ABC/ABC305B.py
@@ -1,5 +1,18 @@
 p, q = input().split()
 
-d = [3, 1, 4, 1, 5, 9]
-i, j = sorted([ord(p) - ord('A'), ord(q) - ord('A')])
-print(sum(d[i:j]))
+# 英文字を数字に変換
+p = ord(p) - ord('A')
+q = ord(q) - ord('A')
+if p > q:
+    p, q = q, p
+# print(p, q)
+
+# スライス和を求めて表示
+line = [3, 1, 4, 1, 5, 9]
+print(sum(line[p:q]))
+
+"""
+スライス和を使った解法
+累積和を使っても解ける
+（別解を参照）
+"""
