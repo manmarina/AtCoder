c1de3a1f952dfa458b3f4651b918cfc1397d8508
manmarina <manmarina@me.com>
2025-09-16 23:36:46 +0900

    347B初回 チャッピーとほぼ同じロジック（スライスの方法が異なる）

--- python/ABC/ABC347B.py
+++ python/ABC/ABC347B.py
@@ -1,9 +1,17 @@
-s = input()
+S = input()
 
-seen = set()
-n = len(s)
-for i in range(n):
-    for j in range(i + 1, n + 1):
-        print(i, j)
-        seen.add(s[i:j])
-print(len(seen))
+ls = len(S)
+ans = set()
+for i in range(1, ls + 1):
+    # print(i, end=':')
+    for j in range(ls + 1 - i):
+        # print(j, end='')
+        ans.add(S[j:j + i])
+    # print()
+
+# print(ans)
+print(len(ans))
+
+"""
+やることはシンプルです。「全部の部分文字列を列挙して set で重複排除 → 個数を数える」だけです。
+"""
