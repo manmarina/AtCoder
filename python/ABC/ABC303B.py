e7ac251fe88aa3edf3678fe02c77919d3260b9f0
manmarina <manmarina@me.com>
2025-09-16 18:44:32 +0900

    303B チャッピー 隣接表を使用した解 ほぼ同じロジック

--- python/ABC/ABC303B.py
+++ python/ABC/ABC303B.py
@@ -1,25 +1,18 @@
 N, M = map(int, input().split())
-a = [list(map(int, input().split())) for _ in range(M)]
-# print(a)
+adj = [[False] * N for _ in range(N)]
 
-friend = [[False] * N for _ in range(N)]
-# print(friend)
+for _ in range(M):
+    A = list(map(int, input().split()))
+    for i in range(N - 1):
+        x, y = A[i] - 1, A[i + 1] - 1  # 0-index化
+        adj[x][y] = adj[y][x] = True
 
-for row in a:
-    for i in range(len(row) - 1):
-        l = row[i] - 1
-        r = row[i + 1] - 1
-        friend[l][r] = True
-        friend[r][l] = True
-# print(friend)
-
-count = 0
+ans = 0
 for i in range(N):
-    for j in range(i + 1, N):  # 隣接表の半分だけ走査
-        if i != j:
-            if not friend[i][j]:
-                count += 1
-print(count)
+    for j in range(i + 1, N):
+        if not adj[i][j]:
+            ans += 1
+print(ans)
 
 """
 無向グラフ
