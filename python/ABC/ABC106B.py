34ae75ad84644c511dbc861c60185a1b1c3cf4eb
manmarina <manmarina@me.com>
2025-09-16 21:27:44 +0900

    106B チャッピー ほぼ同じロジック

--- python/ABC/ABC106B.py
+++ python/ABC/ABC106B.py
@@ -1,13 +1,27 @@
 N = int(input())
 
-count = 0
-for i in range(1, N + 1, 2):
-    div = set()
-    for j in range(1, int(i**0.5) + 1):
-        if i % j == 0:
-            div.add(j)
-            div.add(i // j)
-    # print(i, div, len(div))
-    if len(div) == 8:
-        count += 1
-print(count)
+
+def count_divisors(n: int) -> int:  # 約数の個数をカウント
+    cnt = 0
+    d = 1
+    while d * d <= n:
+        if n % d == 0:
+            cnt += 1                # d
+            if d * d != n:
+                cnt += 1            # n//d（平方でないならペアをもう1つ）
+        d += 1
+    return cnt
+
+
+ans = 0
+for x in range(1, N + 1, 2):        # 奇数だけ
+    if count_divisors(x) == 8:
+        ans += 1
+print(ans)
+
+"""
+奇数に限定して全探索
+    偶数はそもそも数えない（ループを 1,3,5,… にするだけで半分カット）。
+約数個数の数え方（√n まで）
+    d を 1..√n で走らせ、d | n なら約数を 1 個（n/d が d と異なるならもう 1 個）足す。
+"""
