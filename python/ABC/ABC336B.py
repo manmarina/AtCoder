e972bdc60a06f41e716328e70a03dc3cbb855871
manmarina <manmarina@me.com>
2025-09-16 22:28:45 +0900

    336B初回 ビット演算

--- python/ABC/ABC336B.py
+++ python/ABC/ABC336B.py
@@ -1,4 +1,14 @@
 N = int(input())
 
-lowbit = N & -N          # 2^k N&-Nは最下位ビットのみが立つ性質がある
-print(lowbit.bit_length() - 1)  # 最下位ビットの長さ−1が末尾の0の長さ
+count = 0
+for i in range(len(bin(N))):
+    if N >> i & 1 == 0:
+        count += 1
+    else:
+        print(count)
+        break
+"""
+ビット演算
+1がでてくるまでビットを右にシフト（2で割り続ける）
+1がでてくるまでのシフト回数が0の数と一致
+"""
