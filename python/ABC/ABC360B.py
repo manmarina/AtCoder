b0c8fec5cc624d46e8bd92f44e4d54a2b084c2a6
manmarina <manmarina@me.com>
2025-09-17 01:26:33 +0900

    360初回 けんちょんのコードでなんとか理解

--- python/ABC/ABC360B.py
+++ python/ABC/ABC360B.py
@@ -1,12 +1,24 @@
 S, T = input().split()
 
-res = False
-for w in range(1, len(S)):          # 1 <= w < |S|
-    for c in range(w):              # 0 <= c < w
-        U = ""
+ans = []
+for w in range(1, len(S)):
+    for c in range(w):  # 0-index
+        temp = ""
         for k in range(c, len(S), w):
-            U += S[k]
-        if U == T:
-            res = True
+            temp += S[k]
+        ans.append(temp)
+# print(ans)
+print("Yes" if T in ans else "No")
 
-print("Yes" if res else "No")
+"""
+問題文がややこしいが、次のように解釈できる。
+
+0-indexed で考える。
+
+整数 0≤c<w≤|S|
+ であって、文字列 S
+ の添字 (0 始まり) が w
+ で割って c
+ 余る部分の文字を連結したときに、T
+ になるものが存在するかを判定せよ。
+"""
