7696ca1a8408247de48b2eca37805a4b3cbe9aee
manmarina <manmarina@me.com>
2025-09-16 12:39:45 +0900

    258B 初回

--- python/ABC/ABC258B.py
+++ python/ABC/ABC258B.py
@@ -1,24 +1,22 @@
-N = int(input().strip())
-A = [input().strip() for _ in range(N)]
+N = int(input())
+A = [input() for _ in range(N)]
+# print(A)
 
 dirs = [(dx, dy) for dx in (-1, 0, 1)
         for dy in (-1, 0, 1) if not (dx == 0 and dy == 0)]
+# print(dirs)
 
-print(A)
-print(dirs)
-
-best = "0"  # すべて '0' でもOKなように初期化
+ans = []
 for i in range(N):
     for j in range(N):
         for dx, dy in dirs:
-            r, c = i, j
-            s = []
+            x, y = i, j
+            temp = []
             for _ in range(N):
-                s.append(A[r][c])
-                r = (r + dx) % N
-                c = (c + dy) % N
-            cur = "".join(s)
-            if cur > best:
-                best = cur
+                temp.append(A[x][y])
+                x = (x + dx) % N
+                y = (y + dy) % N
+            ans.append(''.join(temp))
 
-print(best)
+# print(ans)
+print(*max(ans), sep='')
