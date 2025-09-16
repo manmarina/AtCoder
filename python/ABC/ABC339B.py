8e21161e698a686d65a1d104424d063863b3202e
manmarina <manmarina@me.com>
2025-09-16 23:01:19 +0900

    339B初回 左右90°回転して移動するカーソル系

--- python/ABC/ABC339B.py
+++ python/ABC/ABC339B.py
@@ -1,24 +1,29 @@
 H, W, N = map(int, input().split())
 
-# 盤：False=白 '.' / True=黒 '#'
-board = [[False] * W for _ in range(H)]
+dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+grid = [['.'] * W for _ in range(H)]
+# print(grid)
 
-r, c = 0, 0          # スタートは (0,0)
-dir = 0              # 0:上, 1:右, 2:下, 3:左
-dr = [-1, 0, 1, 0]
-dc = [0, 1, 0, -1]
-
+# 問題文の通りに色を変えながら向きを変えて進む
+x, y = 0, 0
+r = 0
 for _ in range(N):
-    if not board[r][c]:        # 白
-        dir = (dir + 1) % 4    # 右を向く
-        board[r][c] = True     # 黒に反転
-    else:                      # 黒
-        dir = (dir + 3) % 4    # 左を向く
-        board[r][c] = False    # 白に反転
-
-    r = (r + dr[dir] + H) % H  # Hを足すのは、負の数の%を-で返す他の言語でも対応できるようにするため
-    c = (c + dc[dir] + W) % W  # pythonなら、HやWを足さなくても同じ動作になる
+    if grid[x][y] == '.':
+        grid[x][y] = '#'
+        r += 1
+    else:
+        grid[x][y] = '.'
+        r -= 1
+    dx, dy = dirs[r % 4]
+    x = (x + dx) % H
+    y = (y + dy) % W
 
 # 出力
-for i in range(H):
-    print(''.join('#' if board[i][j] else '.' for j in range(W)))
+for row in grid:
+    print(*row, sep='')
+
+"""
+カーソル系
+向きを表す配列dirを作成
+左右90°回転をインデックスを進めることで表現
+"""
