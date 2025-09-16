24f6f6168bab4c416dcb823764a5ed0b041bc75b
manmarina <manmarina@me.com>
2025-09-17 01:11:37 +0900

    360B けんちょん 少し理解が進んだ

--- python/ABC/ABC360B.py
+++ python/ABC/ABC360B.py
@@ -1,20 +1,12 @@
 S, T = input().split()
-n, m = len(S), len(T)
 
-for c in range(1, n):           # 1 ≤ c
-    for w in range(c, n):        # c ≤ w < n
-        # 取り出し位置は c-1, c-1+w, c-1+2w, ...
-        # 長さが違えばスキップ（任意の枝刈り）
-        k = (n - c) // w + 1
-        if k != m:
-            continue
+res = False
+for w in range(1, len(S)):          # 1 <= w < |S|
+    for c in range(w):              # 0 <= c < w
+        U = ""
+        for k in range(c, len(S), w):
+            U += S[k]
+        if U == T:
+            res = True
 
-        made = []
-        p = c - 1
-        while p < n:
-            made.append(S[p])
-            p += w
-        if ''.join(made) == T:
-            print("Yes")
-            exit()
-print("No")
+print("Yes" if res else "No")
