def279beb47445620811e5bcc5997c582fad25f0
manmarina <manmarina@me.com>
2025-09-18 19:02:50 +0900

    096C チャッピー ほぼ同じロジック

--- python/ABC/ABC096C.py
+++ python/ABC/ABC096C.py
@@ -1,29 +1,31 @@
 H, W = map(int, input().split())
-s = [input() for _ in range(H)]
-# print(s)
+S = [input().strip() for _ in range(H)]
 
-dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
 for i in range(H):
     for j in range(W):
-        if s[i][j] == '#':
-            for dx, dy in dirs:
-                x = i + dx
-                y = j + dy
-                if not (0 <= x < H and 0 <= y < W):
-                    continue
-                if s[x][y] == '#':
-                    break
-            else:  # '#'を囲む4方向に'#'がなければ早期終了
-                print("No")
-                exit()
-else:
-    print("Yes")
+        if S[i][j] != '#':
+            continue
+        ok = False
+        for dx, dy in dirs:
+            ni, nj = i + dx, j + dy
+            if 0 <= ni < H and 0 <= nj < W and S[ni][nj] == '#':
+                ok = True
+                break
+        if not ok:  # 孤立
+            print("No")
+            exit()
+print("Yes")
 
 """
 カーソル系
-DFSを使わない自力解
+DFSを使わない版
 
 上下左右に隣り合う'#'があればよいので、
 全探索して孤立する'#'が見つかったら早期終了
+
+グリッド内の 各 # マスに、上下左右のいずれかへ隣接する # が少なくとも1つあるか をチェック。
+1つでも“孤立した#（上下左右すべて.または外）”があれば No、なければ Yes。
+斜めは 数えません。
 """
