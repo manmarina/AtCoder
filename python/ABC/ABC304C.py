2e996925b162b3fd40a311405a5f6117833b3390
manmarina <manmarina@me.com>
2025-09-19 17:44:49 +0900

    304C けんちょん 再帰DFS グラフを作成せず、dfsで直接感染距離かどうか計算する点が初回コードと異なる

--- python/ABC/ABC304C.py
+++ python/ABC/ABC304C.py
@@ -1,41 +1,50 @@
-from itertools import combinations
-from math import hypot
+import sys
+sys.setrecursionlimit(1 << 25)
 
-N, D = map(int, input().split())
-XY = [tuple(map(int, input().split())) for _ in range(N)]
-# print(XY)
+# 入力
+N, D = map(int, sys.stdin.readline().split())
+X = [0] * N
+Y = [0] * N
+for i in range(N):
+    x, y = map(int, sys.stdin.readline().split())
+    X[i] = x
+    Y[i] = y
 
-# 感染距離内ならグラフに追加
-G = [[] for _ in range(N + 1)]  # 1-index
-for com1, com2 in combinations(enumerate(XY, 1), 2):
-    i, (x1, y1) = com1
-    j, (x2, y2) = com2
-    if hypot(x1 - x2, y1 - y2) <= D:
-        G[i].append(j)
-        G[j].append(i)
-# print(G)
+D2 = D * D  # Dの二乗を前計算
 
 
-# dfs開始準備
-seen = [False] * (N + 1)  # 1-index
+def is_connected(i: int, j: int) -> bool:
+    """
+    頂点 i, j 間が連結かどうか
+    """
+    dx = X[i] - X[j]
+    dy = Y[i] - Y[j]
+    return dx * dx + dy * dy <= D2
 
-# dfs部分
-seen[1] = True
-stack = [1]
-while stack:
-    v = stack.pop()
-    for nv in G[v]:
-        if not seen[nv]:
-            seen[nv] = True
-            stack.append(nv)
 
-# 結果出力
-for infected in seen[1:]:
-    print("Yes" if infected else "No")
+# DFS
+seen = [False] * N
+
+
+def dfs(v: int) -> None:
+    seen[v] = True
+    for v2 in range(N):
+        if seen[v2]:
+            continue
+        if is_connected(v, v2):
+            dfs(v2)
+
+
+# 実行
+dfs(0)
+
+# 出力
+for v in range(N):
+    print("Yes" if seen[v] else "No")
 
 """
-反復DFS
-284Cのコードを参照して自力で実装。
+再帰DFS
+けんちょん
 
 この問題は、まず「グラフの問題だと思えるか」どうかが一つの壁になるかもしれないですね。
 
@@ -44,4 +53,6 @@ for infected in seen[1:]:
 としたグラフを考えましょう。
 こうして作ったグラフにおいて、各頂点が頂点 1 とつながっているかどうかを調べればよいです。
 （けんちょん）
+
+グラフを作成せず、dfsで直接感染距離かどうか計算する点が初回コードと異なる
 """
