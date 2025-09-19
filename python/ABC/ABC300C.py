58a0836faa8b41d94785c77c504279c94a5e41d9
manmarina <manmarina@me.com>
2025-09-19 13:20:46 +0900

    300C けんちょん BFS

--- python/ABC/ABC300C.py
+++ python/ABC/ABC300C.py
@@ -1,5 +1,5 @@
 import sys
-sys.setrecursionlimit(1 << 25)
+from collections import deque
 
 # 入力
 H, W = map(int, sys.stdin.readline().split())
@@ -11,18 +11,22 @@ dxs = [1, 1, -1, -1]
 dys = [1, -1, 1, -1]
 
 
-def dfs(i, j):
+def bfs(si, sj):
     """
-    DFS: '#' の個数を返す
+    BFS: '#' の個数を返す
     また、探索済みの `#` マスは `.` に塗りつぶす
     """
+    C[si][sj] = '.'
+    q = deque([(si, sj)])
     cnt = 1
-    C[i][j] = '.'  # 訪問済みにする
-    for d in range(4):
-        ni = i + dxs[d]
-        nj = j + dys[d]
-        if 0 <= ni < H and 0 <= nj < W and C[ni][nj] == '#':
-            cnt += dfs(ni, nj)  # グリッド範囲外でなく#なら再帰dfs
+    while q:
+        x, y = q.popleft()
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < H and 0 <= ny < W and C[nx][ny] == '#':
+                C[nx][ny] = '.'
+                q.append((nx, ny))
+                cnt += 1
     return cnt
 
 
@@ -34,7 +38,7 @@ for i in range(H):
     for j in range(W):
         if C[i][j] == '.':
             continue
-        num = dfs(i, j)
+        num = bfs(i, j)
         # レベルは '#' の個数を 4 で割った商 - 1
         # （元コードのロジックを忠実に移植）
         res[(num // 4) - 1] += 1
@@ -42,7 +46,7 @@ for i in range(H):
 print(*res)
 
 """
-再帰DFS
+BFS
 けんちょんの実装
 グリッド上の「X字型（対角方向に伸びる十字）」の大きさを数える問題です。
 
