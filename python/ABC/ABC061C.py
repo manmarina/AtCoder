2d7415ea9131532b4927397d49cb8c4fc29adfe9
manmarina <manmarina@me.com>
2025-09-20 14:15:47 +0900

    061C チャッピー 累積和（prefix和） abを連想配列として格納する点が自力解と異なる

--- python/ABC/ABC061C.py
+++ python/ABC/ABC061C.py
@@ -1,20 +1,27 @@
+import sys
+
+input = sys.stdin.readline
 N, K = map(int, input().split())
-ab = [tuple(map(int, input().split()))for _ in range(N)]
-# print(ab)
 
-# abをaをキーに昇順でソートしておく
-ab.sort(key=lambda x: x[0])
+# abを連想配列として格納
+cnt = {}
+for _ in range(N):
+    a, b = map(int, input().split())
+    cnt[a] = cnt.get(a, 0) + b  # 合算
+# print(cnt)
 
-cs = 0
-for a, b in ab:
-    cs += b
-    if cs >= K:  # b の累積和がKを超えた時 a を表示して終了
+cur = 0
+for a in sorted(cnt):           # a 昇順
+    cur += cnt[a]               # 累積
+    if cur >= K:                # 初めて K に到達/超過
         print(a)
         break
 
+
 """
 累積和（prefix和）
-自力解
+チャッピー
+abを連想配列として格納する点が自力解と異なる
 
  狙いはシンプルです：
 巨大配列を作る代わりに、
