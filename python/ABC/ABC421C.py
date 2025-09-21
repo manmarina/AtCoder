b3a6b29962c32911dba84872c2ad779acb985d21
manmarina <manmarina@me.com>
2025-09-21 10:46:00 +0900

    421C チャッピー 発想の転換系 公式動画の解説がとても参考になった！！

--- python/ABC/ABC421C.py
+++ python/ABC/ABC421C.py
@@ -1,38 +1,34 @@
+import sys
+
+input = sys.stdin.readline
 N = int(input())
-S = [*input()]
+S = input().strip()
 
-# print(S)
-# print(ok)
+posA = [i for i, ch in enumerate(S) if ch == 'A']  # 長さN
+# print(posA)
 
-count = 0
-for i in range(N * 2 - 2):
-    if S[i] == 'A':
-        dist = 0
-        for j in range(i + 1, N * 2):
-            if S[j] == 'B':
-                break
-            dist += 1
+# Aを偶数番(0,2,4,...)に置く場合
+cost_even = 0
+for k, i in enumerate(posA):
+    cost_even += abs(i - 2 * k)
 
-        # print(dist)
+# Aを奇数番(1,3,5,...)に置く場合
+cost_odd = 0
+for k, i in enumerate(posA):
+    cost_odd += abs(i - (2 * k + 1))
 
-        count += dist
-        S[i + 1] = 'B'
-        if dist != 0:
-            S[i + dist + 1] = 'A'
+print(min(cost_even, cost_odd))
 
-    else:  # S[i] == 'B'
-        dist = 0
-        for j in range(i + 1, N * 2):
-            if S[j] == 'A':
-                break
-            dist += 1
+"""
+発想の転換系
+チャッピー
+公式解説動画を見て理解
 
-        # print(dist)
+N 個の A と N 個の B からなり同じ文字が隣り合う箇所がない文字列は ABABAB..., BABABA... の 2 種類しかありません。
+よって「 S を T にするには何回の操作が必要か？」というタイプの問題を 2 回解けば元の問題に答えることができます。
 
-        count += dist
-        S[i + 1] = 'A'
-        if dist != 0:
-            S[i + dist + 1] = 'B'
-
-# print(S)
-print(count)
+同じ文字が隣り合っている箇所に対して操作をする意味はありません。よって、操作の前後で A の順序が入れ替わることはありません。つまり、
+ここがポイント！！
+S の前から k 番目にある A は、T の前から k 番目にある Aの位置に移動します。
+https://atcoder.jp/contests/abc421/editorial/13730
+"""
