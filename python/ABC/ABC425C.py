9909988d4af633a8b72dda565cc259a11be1b0ff
manmarina <manmarina@me.com>
2025-09-27 22:06:26 +0900

    425C TLE

--- python/ABC/ABC425C.py
+++ python/ABC/ABC425C.py
@@ -4,9 +4,20 @@ query = [list(map(int, input().split())) for _ in range(Q)]
 # print(A)
 # print(query)
 
+cs = [0] + [A[0]]
+for i in range(1, N):
+    cs.append(cs[i] + A[i])
+# print(cs)
+
 for q in query:
     if q[0] == 1:
         A = A[q[1]:] + A[:q[1]]
         # print(A)
+
+        cs = [0] + [A[0]]
+        for i in range(1, N):
+            cs.append(cs[i] + A[i])
+        # print(cs)
+
     else:
-        print(sum(A[q[1] - 1:q[2]]))
+        print(cs[q[2]] - cs[q[1] - 1])
