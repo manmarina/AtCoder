967d16f6ba99f4e5527b1d3e3ad07f8ca7b50c8d
manmarina <manmarina@me.com>
2025-10-01 23:32:49 +0900

    246C初回 WA 貪欲法

---
+++ python/ABC/ABC246C.py
@@ -0,0 +1,38 @@
+N, K, X = map(int, input().split())
+A = list(map(int, input().split()))
+
+A.sort(reverse=True)
+# print(A)
+ans = []
+for a in A:
+    if K > 0:
+        div, mod = divmod(a, X)
+        if div < K:
+            ans.append(mod)
+            K -= div
+        else:
+            ans.append(a - K * X)
+            K = 0
+# print(ans)
+# print(sum(ans))
+# print(K)
+
+ans.sort(reverse=True)
+# print(ans)
+for i, a in enumerate(ans):
+    if K > 0:
+        ans[i] = 0
+        K -= 1
+# print(ans)
+print(sum(ans))
+
+"""
+貪欲法（Greedy）+ソート
+自力解 WA
+
+クーポン1枚で「好きなAiからXを引く（負にならない）」。
+合計K枚で配列の総和を最小にしたい。
+
+https://atcoder.jp/contests/abc246/submissions/69770080
+https://chatgpt.com/g/g-p-688d3155796881919ed997146b54eec1-atcoder/c/68dd347d-1d94-832d-8fcc-d5303aca53b1
+"""
