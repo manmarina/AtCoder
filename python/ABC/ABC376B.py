9f805533de939e7034ee284253a141af7f640b34
manmarina <manmarina@me.com>
2025-10-22 16:26:07 +0900

    376B 解説方法 場合分けを減らす

--- python/ABC/ABC376B.py
+++ python/ABC/ABC376B.py
@@ -1,37 +1,36 @@
-N, Q = map(int, input().split())
-HT = [list(input().split()) for _ in range(Q)]
-
-L, R = 1, 2  # 初期位置
-
-
-def cw(a, b):            # 時計回り距離
-    return (b - a + N) % N
-
-
-def in_cw_arc(a, b, x):
-    """a→b の時計回り区間に x があるか（a除外, b含む）
-       ※a==b のときは空区間（何も含まない）"""
-    if a == b:
-        return False
-    if a < b:
-        return a < x <= b
+def num_move(n, from_, to, ng):
+    if from_ > to:  # l > tのときは
+        from_, to = to, from_  # l < tとしてやっても問題ない（距離がわかればよいので）
+    if from_ < ng < to:  # 反対の手が間にいるとき
+        return n - (to - from_)  # n - 近い方
     else:
-        return x > a or x <= b
+        return to - from_  # 近い方
 
 
+n, q = map(int, input().split())
+l, r = 1, 2
 ans = 0
-for h, t in HT:
+
+for _ in range(q):
+    h, t = input().split()
     t = int(t)
     if h == 'L':
-        p, s = L, R
-        d = cw(p, t)
-        # s が CW 区間にいなければ CW、いれば CCW
-        ans += d if not in_cw_arc(p, t, s) else (N - d)
-        L = t
+        ans += num_move(n, l, t, r)
+        l = t
     else:
-        p, s = R, L
-        d = cw(p, t)
-        ans += d if not in_cw_arc(p, t, s) else (N - d)
-        R = t
+        ans += num_move(n, r, t, l)
+        r = t
 
 print(ans)
+
+"""
+場合分けを減らす
+解説放送
+
+l -> Tの移動を考えるとき
+距離がわかればよいだけなので、
+l > Tだとしても、入れ替えて考えても問題ない。
+すると、
+l < T の間に rが入るかどうかだけ考えればよくなる。
+つまり、間にrが入らない方向の距離だけ足してやればよい。
+"""
