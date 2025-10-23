5193b1bf81dca09dca5689ed12dc0fb21b4bf5e1
manmarina <manmarina@me.com>
2025-10-23 13:37:15 +0900

    381C 解説 文字列操作

--- python/ABC/ABC381C.py
+++ python/ABC/ABC381C.py
@@ -1,27 +1,36 @@
 N = int(input())
 T = input()
 
+ans = []
+for i in range(N):
+    # '/'でなければ次の文字へ
+    if T[i] != '/':
+        continue
 
-def is_1122(t):
-    lt = len(t)
-    if lt % 2 == 0:  # ltは奇数である。
-        return False
+    # '/'だったとき
+    cnt = 1
+    for j in range(1, N):
+        if i - j < 0 or i + j > N - 1:  # 文字列の範囲外のときは次の文字へ
+            break
+        if T[i - j] != '1' or T[i + j] != '2':  # '/'の左が'1'、右が'2'でないときは次の文字へ
+            break
+        cnt += 2
 
-    pos = (lt + 1) // 2 - 1  # (lt + 1) // 2 文字目が '/' である。
-    if t[pos] != '/':
-        return False
+    # cntをansに追加
+    ans.append(cnt)
 
-    for i in range(pos):  # 1 文字目から (lt + 1) // 2 - 1 文字目までが 1 である。
-        if t[i] != '1':
-            return False
+# print(ans)
+print(max(ans) if ans else 0)  # ansが空の時の処理を書かないとRE
 
-    for i in range(
-        (lt + 1) // 2,
-            lt):  # (lt + 1) // 2 + 1 文字目から lt 文字目までが 2 である。
-        if t[i] != '2':
-            return False
+"""
+文字列操作
+解説
+11/22文字列の最長の長さを探索する。
 
-    return True
+この操作では 1 回の繰り返しにつき最悪 O(N) の計算量が掛かるので
+O(N^2) になってしまうと考える方もいるかもしれませんが、
+実は計算量は O(N)で済みます
 
-
-print(is_1122(T))
+https://atcoder.jp/contests/abc381/tasks/abc381_c
+https://chatgpt.com/g/g-p-688d3155796881919ed997146b54eec1-atcoder/c/68f9a79b-d150-8321-ae45-60f624ce5cd4
+"""
