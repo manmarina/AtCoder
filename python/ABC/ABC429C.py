092eb2fedb7cc48f564982cafe0c13505f81fa2d
manmarina <manmarina@me.com>
2025-10-25 22:01:50 +0900

    429C loggingを削除してAC C問題初AC!!

--- python/ABC/ABC429C.py
+++ python/ABC/ABC429C.py
@@ -1,16 +1,10 @@
 from collections import Counter
-import logging
-logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] %(message)s')
-# logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
-
 
 N = int(input())
 A = list(map(int, input().split()))
 
 cnt1 = Counter(A)
-logging.debug(f"{cnt1 = }")
 cnt2 = [v for v in cnt1.values()]
-logging.debug(f"{cnt2 = }")
 
 
 def nC2(n):
@@ -25,9 +19,4 @@ for c in cnt2:
         count = total_count - c
         ans += combi * count
 
-logging.debug(f"{combi = },{count = }")
 print(ans)
-
-"""
-RE
-"""
