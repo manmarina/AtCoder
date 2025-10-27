277fb0c83ca9fe1a4cfc1689978e6a80db9fb396
manmarina <manmarina@me.com>
2025-10-27 12:34:49 +0900

    416C yuulis 全探索 + dfs

--- python/ABC/ABC416C.py
+++ python/ABC/ABC416C.py
@@ -5,15 +5,28 @@ N, K, X = map(int, input().split())
 S = [input() for _ in range(N)]
 
 ans = []
-for combi in product(S, repeat=K):  # N^Kパターンの組み合わせ
-    conc = ''.join(combi)  # 文字列を結合
-    ans.append(conc)  # 結合した文字列をリストに追加
+
+
+def dfs(curr, count):  # (文字列,結合回数)
+    # count 個の文字列を結合して curr になった状態
+    if count == K:  # 結合回数がK回になったら
+        ans.append(curr)  # リストに文字列を追加
+        return
+    for s in S:  # S から一つずつ取り出して再帰dfs
+        dfs(curr + s, count + 1)  # (文字列を連結、結合回数を+1)
+
+
+dfs("", 0)  # (空文字、count=0)からスタート
 ans.sort()  # リストを昇順ソート
 print(ans[X - 1])  # 辞書順でX番目を表示
 
 """
-全探索
-組み合わせ生成にdfsを使っても解けるらしい。
+全探索 + dfs
+組み合わせ生成にdfsを使用
+各頂点にN個の文字列いずれかが書かれた、""を根とする高さKのN分木を考える
+https://yuulis.hatenablog.com/entry/ABC-416-C
+https://atcoder.jp/contests/abc416/editorial/13535
 
 https://atcoder.jp/contests/abc416/tasks/abc416_c
+https://chatgpt.com/g/g-p-688d3155796881919ed997146b54eec1-atcoder/project
 """
