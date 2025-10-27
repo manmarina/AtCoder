e0ea4c23a96866bcfa42de6e541f785afae82f98
manmarina <manmarina@me.com>
2025-10-27 23:54:30 +0900

    180C けんちょん 約数列挙

--- python/ABC/ABC180C.py
+++ python/ABC/ABC180C.py
@@ -1,17 +1,23 @@
 N = int(input())
 
-ans = set()  # リストだと、約数に平方根があると同じ数字が重複してWAになる
-for i in range(1, int(N**0.5) + 1):  # 平方根まで探索
+res = []
+i = 1
+while i * i <= N:
     if N % i == 0:
-        ans.add(i)
-        ans.add(N // i)
-ans = sorted(ans)
-print(*ans, sep='\n')
+        res.append(i)
+        if i * i != N:  # 平方根でなければ
+            res.append(N // i)
+    i += 1
 
+res.sort()
+for v in res:
+    print(v)
 
 """
 約数列挙
+けんちょん
 完全に約数列挙！！！！！
+setを使わないなら、appendする前に、重複チェックをする。
 
 https://atcoder.jp/contests/abc180/tasks/abc180_c
 https://drken1215.hatenablog.com/entry/2020/10/21/194700
