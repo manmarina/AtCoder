b35e0becba688bfbe27377eb6ccf4c8457498095
manmarina <manmarina@me.com>
2025-10-29 23:05:14 +0900

    278C チャッピー 計算量を削減したクエリ処理

--- python/ABC/ABC278C.py
+++ python/ABC/ABC278C.py
@@ -1,23 +1,20 @@
 N, Q = map(int, input().split())
 TAB = [list(map(int, input().split())) for _ in range(Q)]
 
-G = [set() for _ in range(N + 1)]
-# print(G)
-
+S = set()
 for t, a, b in TAB:
     if t == 1:
-        G[a].add(b)
+        S.add((a, b))
     elif t == 2:
-        G[a].discard(b)  # 存在しなくてもOK
+        S.discard((a, b))  # erase に対応（存在しなくてもOK）
     else:  # t == 3:
-        if b in G[a] and a in G[b]:
+        if (a, b) in S and (b, a) in S:
             print("Yes")
         else:
             print("No")
-# print(G)
 
 """
-RE & TLE
+計算量を削減したクエリ処理
 
 チャッピーによる解説
 https://chatgpt.com/g/g-p-688d3155796881919ed997146b54eec1-atcoder/c/690218b0-766c-8322-b574-4399935762e2
@@ -25,6 +22,9 @@ https://chatgpt.com/g/g-p-688d3155796881919ed997146b54eec1-atcoder/c/690218b0-76
 メモリが膨らんで MemoryError→RE になる可能性がある。
 計算量はすべてO(1)だが、メモリ使用量が逼迫してTLEも併発しているかもしれない。
 -> setはひとつにして、タプルでデータを追加すればメモリ使用量を削減できる。
+
+けんちょんの解説
+https://drken1215.hatenablog.com/entry/2024/11/09/155033
 
 https://atcoder.jp/contests/abc278/tasks/abc278_c
 """
