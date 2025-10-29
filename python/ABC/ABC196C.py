e94d3a23ad14a40a34f20e73afa34a24115bd470
manmarina <manmarina@me.com>
2025-10-29 11:15:20 +0900

    196C 解説 問題を言い換える系

--- python/ABC/ABC196C.py
+++ python/ABC/ABC196C.py
@@ -1,28 +1,16 @@
-def reconstruct(n):
-    # '123'を’123123'に変換する関数
-    val = 1
-    nn = n
-    while nn:
-        val *= 10  # 一番大きい桁を切り上げる ex.'123' -> '1000'
-        nn //= 10
-    return n * val + n
-
-
 N = int(input())
-res = 0
-
-for n in range(1, 1_000_001):
-    if reconstruct(n) <= N:
-        res += 1
-    else:
-        break
-
-print(res)
+for i in range(1, 1000001):
+    if int(str(i) * 2) > N:  # 文字列に変換して繰り返してから整数に戻す
+        print(i - 1)  # Nを超える手前までの数が整数xの個数
+        exit()
 
 """
 問題を言い換える系
-けんちょん
-https://drken1215.hatenablog.com/entry/2021/03/20/225100
+解説
+https://atcoder.jp/contests/abc196/editorial/946
+けんちょんよりもシンプルなコード。
+ロジックはけんちょんと同じ。
+
 たとえば 1234512345 は「良い整数」だが、これは「12345」という整数と同一視できる。
 このように圧縮した整数の方を全探索すればよいことに気づく。
 
