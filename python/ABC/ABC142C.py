e5bf3a9ba631120289e5b1ac7ce59cf87eb8b052
manmarina <manmarina@me.com>
2025-10-31 11:29:28 +0900

    142C けんちょん 逆順列

--- python/ABC/ABC142C.py
+++ python/ABC/ABC142C.py
@@ -1,14 +1,18 @@
 N = int(input())
 A = list(map(int, input().split()))
-B = []
-for i in range(1, N + 1):
-    B.append((A[i - 1], i))  # (登校した順番、生徒番号)のリストを作成
-B.sort()  # 登校した順番でソート
-# print(B)
 
+# 0-index にする
+A = [x - 1 for x in A]  # 生徒番号 -> 投稿した順
+print(A)
+
+B = [0] * N  # 空の配列を作成しておくのがポイント
 for i in range(N):
-    print(B[i][1], end=' ')  # 投稿した順に生徒番号を出力
-print()
+    # B[インデックスを値に] = 値をインデックスに
+    B[A[i]] = i  # 逆順列に変換 登校した順 -> 生徒番号
+print(B)
+
+# 出力時は 1-index に戻す
+print(*[x + 1 for x in B])
 
 """
 逆順列
@@ -18,4 +22,5 @@ print()
 https://drken1215.hatenablog.com/entry/2019/10/03/003600
 
 https://atcoder.jp/contests/abc142/tasks/abc142_c/editorial
+https://chatgpt.com/g/g-p-688d3155796881919ed997146b54eec1-atcoder/c/69041a9a-107c-8324-ae63-931b7f29e2fd
 """
