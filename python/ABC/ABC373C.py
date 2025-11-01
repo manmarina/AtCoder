c00b4b98e6fb9d4e4f14b9449a5b884d7219ee3e
manmarina <manmarina@me.com>
2025-11-01 16:04:09 +0900

    373C初回 基本実装問題

---
+++ python/ABC/ABC373C.py
@@ -0,0 +1,15 @@
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+print(max(A) + max(B))
+
+"""
+基本実装問題
+Max Ai+ MAX Bjを求める問題。
+
+けんちょんの解説
+https://drken1215.hatenablog.com/entry/2024/09/29/010554
+
+https://atcoder.jp/contests/abc373/tasks/abc373_c
+"""
