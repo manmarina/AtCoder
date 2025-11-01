418b9bd4bdb97f55d5cd3e1e3eed42a660d7854d
manmarina <manmarina@me.com>
2025-11-01 18:38:32 +0900

    159C初回 数学的な気づき系

---
+++ python/ABC/ABC159C.py
@@ -0,0 +1,14 @@
+L = int(input())
+
+print((L/3) ** 3)
+
+"""
+数学的な気づき系
+この手の問題は、a=b=cのとき最大になることが多い。
+つまり、a=L/3, b=L/3, c=L/3のときに abcは最大となる。
+
+けんちょんの解説
+https://drken1215.hatenablog.com/entry/2024/11/08/000758
+
+https://atcoder.jp/contests/abc159/tasks/abc159_c/editorial
+"""
