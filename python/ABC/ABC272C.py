874667f86e5f651f2930ad1925e8f0973f8e3a62
manmarina <manmarina@me.com>
2025-11-03 15:58:38 +0900

    272C WA

--- python/ABC/ABC272C.py
+++ python/ABC/ABC272C.py
@@ -4,16 +4,43 @@ A = list(map(int, input().split()))
 A.sort(reverse=True)
 # print(A)
 
-mx = A[0]
-for i in range(1, N):
-    if (mx + A[i]) % 2 == 0:
-        print(mx + A[i])
-        exit()
-else:
-    print(-1)
+even = []
+for i in range(N):
+    if A[i] % 2 == 0:
+        even.append(A[i])
+        if len(even) == 2:
+            break
+odd = []
+for i in range(N):
+    if A[i] % 2 == 1:
+        odd.append(A[i])
+        if len(odd) == 2:
+            break
+
+
+if len(even) <= 1:
+    even = []
+
+if len(odd) <= 1:
+    odd = []
+
+# print(even)
+# print(odd)
+
+print(max(sum(even), sum(odd)))
 
 """
-WA
+数学的な気づき系 WA
+偶奇を考える
+
+偶数の和は：
+偶数 + 偶数
+奇数 + 奇数
+で作れます。
+したがって、
+偶数の中で大きい2つ
+奇数の中で大きい2つ
+をそれぞれ求めて、それらの和のうち最大を答えればOKです。
 
 https://atcoder.jp/contests/abc272/tasks/abc272_c
 https://chatgpt.com/g/g-p-688d3155796881919ed997146b54eec1-atcoder/c/69084f3c-9eb0-8321-aa28-f9d84893c5e5
