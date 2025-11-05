d4409d210d5aef52e5f4dd234832d9e0b97ee5e7
manmarina <manmarina@me.com>
2025-11-05 19:57:35 +0900

    326C けんちょん 二分探索

--- python/ABC/ABC326C.py
+++ python/ABC/ABC326C.py
@@ -1,31 +1,26 @@
+import bisect
+
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
-print(A)
+A.sort()
 
-B = [0] * (max(A) + 1)
-for a in A:
-    B[a] += 1
-print(B)
+res = 0
+for i in range(N):  # 左端を決める
+    x = A[i]  # 左端の座標
+    # x + M以上となるインデックス -> x + Mより小さいインデックスはbisect_left()-1
+    it = bisect.bisect_left(A, x + M)  # 右端の座標（it-1）
 
-cs = [0] * (max(A) + 1)
-for i in range(1, len(cs)):
-    cs[i] = cs[i - 1] + B[i]
-print(cs)
+    res = max(res, it - i)  # it - 1 - (i - 1) 区間に入る要素の個数
 
-ans = 0
-for i in range(1, len(cs) - M + 1):
-    # print('i:', i, "i+M-1:", i + M - 1)
-    ans = max(ans, cs[i + M - 1] - cs[i - 1])
-
-print(ans)
-
+print(res)
 
 """
-TLE
-配列のサイズが大きすぎる!（最大10^9）
+二分探索
+二分探索（bisect_left()）の教育的問題
 
 けんちょん
 https://drken1215.hatenablog.com/entry/2023/11/11/204912
 
 https://atcoder.jp/contests/abc326/tasks/abc326_c
+https://chatgpt.com/g/g-p-688d3155796881919ed997146b54eec1-atcoder/project
 """
