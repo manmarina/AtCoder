894bef8d5ddf0497c45f567d724cd38a28279ad2
manmarina <manmarina@me.com>
2025-11-05 10:05:51 +0900

    261C リトライ 文字列処理 x 連想配列（ハッシュマップ）

--- python/ABC/ABC261C.py
+++ python/ABC/ABC261C.py
@@ -1,23 +1,22 @@
-import sys
+from collections import defaultdict
 
-input = sys.stdin.readline
-n = int(input())
-cnt = {}  # s -> 出現回数
-out = []
-for _ in range(n):
-    s = input().rstrip("\n")
-    k = cnt.get(s, 0)
-    if k == 0:
-        out.append(s)
+
+N = int(input())
+S = [input() for _ in range(N)]
+# print(S)
+
+dd = defaultdict(int)
+for i in range(N):
+    cnt = dd[S[i]]
+    if cnt == 0:
+        print(S[i])
+        dd[S[i]] += 1
     else:
-        out.append(f"{s}({k})")
-    cnt[s] = k + 1
-print("\n".join(out))
-
+        print(f"{S[i]}({dd[S[i]]})")
+        dd[S[i]] += 1
 
 """
 文字列処理 x 連想配列（ハッシュマップ）
-チャッピー
 
 https://atcoder.jp/contests/abc261/tasks/abc261_c
 https://chatgpt.com/g/g-p-688d3155796881919ed997146b54eec1-atcoder/c/68de2fb5-575c-8321-89a9-d45ab3b032c
