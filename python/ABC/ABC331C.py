c85c1b9156b5af68a16902510e134e076273250e
manmarina <manmarina@me.com>
2025-11-08 13:58:32 +0900

    331C 計算量を削減したシミュレーション + ランレングス圧縮

--- python/ABC/ABC331C.py
+++ python/ABC/ABC331C.py
@@ -7,13 +7,34 @@ A = list(map(int, input().split()))
 B = sorted(A)
 # print(B)
 
-dd = defaultdict(int)
-for i in range(N - 1):
-    if B[i] != B[i + 1]:
-        dd[B[i]] = sum(B[i + 1:])
+# ランレングス圧縮
+lens = []
+i = 0
+while i < N:
+    j = i
+    while j < N and B[j] == B[i]:
+        j += 1
+    lens.append((B[i], j - i))  # (文字,文字長)を記録
+    i = j
+# print(lens)
+
+# ddの作成
+sum_B = sum(B)  # Bの全体のsumを計算しておく
+dd = defaultdict(int)  # Ai -> Aiより大きな要素の和
+for v, n in lens:  # (文字,文字長)を取り出す
+    sum_B -= v * n  # ある文字よりも大きい要素の和を求める
+    dd[v] = sum_B
 # print(dd)
 
+# 出力
 out = []
 for a in A:
     out.append(dd[a])
 print(*out)
+
+"""
+計算量を削減したシミュレーション + ランレングス圧縮
+ランレングス圧縮を活用して連想配列の生成を高速化。
+
+https://atcoder.jp/contests/abc331/tasks/abc331_c
+"""
