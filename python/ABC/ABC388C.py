eb6804d60b98a04c053697ba018671c9fa34ebed
manmarina <manmarina@me.com>
2025-11-10 02:09:20 +0900

    388C 解説+チャッピー 計算量を削減したシミュレーション + しゃくとり法

--- python/ABC/ABC388C.py
+++ python/ABC/ABC388C.py
@@ -1,20 +1,25 @@
-from bisect import bisect_left
-
-
 N = int(input())
 A = list(map(int, input().split()))
 
 ans = 0
-for i in range(N):
-    idx = bisect_left(A, A[i] * 2)  # 2倍の大きさのもちのインデックス
-    # print(i, idx, N - idx)
-    ans += N - idx  # 2倍の大きさのもちの数
+
+# a / 2 より大きい最初の要素（なければ最後の次）を表す値 j
+j = 0
+
+for a in A:
+    # 越えるまで進める
+    while j < N and A[j] * 2 <= a:
+        j += 1  # 自分の大きさの半分以下の餅の数
+    ans += j
+
 print(ans)
 
 """
-計算量を削減したシミュレーション + 二分探索
-自分の大きさの倍以上のもちの数を二分探索で高速に求める。
-しゃくとり法でも解けるらしい。
+計算量を削減したシミュレーション + しゃくとり法
+解説+チャッピー
+二分探索では、自分の大きさの倍以上のもちの数を求めたが、
+しゃくとり法では、自分の大きさの半分以下の餅の数を求めてゆく。
 
 https://atcoder.jp/contests/abc388/tasks/abc388_c
+https://chatgpt.com/g/g-p-688d3155796881919ed997146b54eec1-atcoder/c/6910c8f2-d858-8323-9214-856f944e1028
 """
