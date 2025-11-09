05fb6764a3495f5920632b3d44b8f3c54806548d
manmarina <manmarina@me.com>
2025-11-09 17:03:19 +0900

    186C けんちょん 数学的な気づき系

--- python/ABC/ABC186C.py
+++ python/ABC/ABC186C.py
@@ -1,35 +1,27 @@
+def ok(v, b):
+    while v:
+        if v % b == 7:
+            return False
+        v //= b
+    return True
+
+
 N = int(input())
+res = 0
 
-# 10進数の確認
-ok = set()  # 7を含む数を格納するset
-for i in range(1, N + 1):  # 1~Nまで
-    for s in str(i):
-        if s == '7':
-            ok.add(i)
+for n in range(1, N + 1):
+    if ok(n, 10) and ok(n, 8):
+        res += 1
 
-
-# 8進数に変換したときに7を含むか判定する関数
-def has_seven(n):
-    while n > 0:
-        if str(n % 8) == '7':
-            return True
-        n //= 8
-    return False
-
-
-# 8進数の確認
-for i in range(1, N + 1):  # 1~Nまで
-    if has_seven(i):
-        ok.add(i)
-
-print(N - len(ok))  # Nから7を含む数の個数を引いたものが答え
+print(res)
 
 """
 数学的な気づき系
 8進数に変換する。
 7が含まれる数をsetに追加してゆく。
+自力解をさらにエレガントに！！
 
-けんちょんの解説
+けんちょん
 https://drken1215.hatenablog.com/entry/2020/12/19/224100
 
 https://atcoder.jp/contests/abc186/tasks/abc186_c
