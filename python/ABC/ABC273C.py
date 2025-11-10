be1cf093e88345ddaebb57f734d04052e275d846
manmarina <manmarina@me.com>
2025-11-11 02:16:39 +0900

    273C リトライ 問題文の理解が難解系

--- python/ABC/ABC273C.py
+++ python/ABC/ABC273C.py
@@ -1,47 +1,37 @@
-# 入力
-n = int(input())
-a_list = list(map(int, input().split()))
+from collections import Counter, defaultdict
 
-# map<int,int> mp と同様に辞書でカウント
-mp = {}
-for a in a_list:
-    mp[a] = mp.get(a, 0) + 1
-# print(mp)
 
-# 降順で出力
-for key in sorted(mp.keys(), reverse=True):
-    print(mp[key])  # Aiより大きいものが0種類のものの個数から順に出力される
+N = int(input())
+A = list(map(int, input().split()))
 
-# 残りを 0 で埋める
-for _ in range(n - len(mp)):
-    print(0)
+# カウンターを作成 Ai -> 個数
+cnt = Counter(A)
+# print(cnt)
 
+# カウンターのキーを降順でソートして一旦リストに(先頭から0種類の個数、1種類の個数,2種類の個数...となる。)
+cnt = sorted(cnt.items(), reverse=True)
+# print(cnt)
+
+# カウンターのキーの名前を0,1,2...に変更して辞書として再作成
+cnt = {i: value for i, (_, value) in enumerate(cnt)}
+# print(cnt)
+
+# 辞書をdefaultdictに変更
+dd = defaultdict(int, cnt)
+# print(dd)
+
+# 0から順に出力
+for i in range(N):
+    print(dd[i])
 
 """
 問題文の理解が難解系
+リトライ
 
-問題文を読んでも何を出力すればよいのかわかりづらい。
-解説放送でも理解できなかった。
-参考になったサイト
-https://programming-hiroba.com/abc273-c/
-
-出力は
-K=0から始まり、K=N-1まで
-(K = 0,1,2,...,N-1)
-
-Aiより大きいものがちょうどK"種類"になるAiの"個数"を出力する
-
-つまり、
-2 7 1 8 2 8
-の場合は、
-8: 2個 -> 8より大きいものは0種類（0行目） -> 2個と出力
-7: 1個 -> 7より大きいものは1種類（1行目） -> 1個と出力
-2: 2個 -> 2より大きいものは2種類（2行目） -> 3個と出力
-1: 1個 -> 1より大きいものは3種類（3行目） -> 4個と出力
-4種類は0個
-5種類は0個
-6種類は0個
-という出力になる。
+1回目より早く何を出力すればよいか理解できた。
+ロジックは前回と似ているが、辞書のキーを直接変更した点が異なる。
+辞書のキーの操作について追加でチャッピーに質問した。
+https://chatgpt.com/g/g-p-688d3155796881919ed997146b54eec1-atcoder/c/6912178c-1098-8323-95f7-603be12b1f31
 
 https://atcoder.jp/contests/abc273/tasks/abc273_c
 https://chatgpt.com/c/68f9cb31-6254-8322-a131-341c7bdff8b7
