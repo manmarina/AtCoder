d26285b34a193e08528910dc2fa312b9b9e493c5
manmarina <manmarina@me.com>
2025-11-10 13:40:05 +0900

    343C リトライ 数学的な気づき系 + 全探索

--- python/ABC/ABC343C.py
+++ python/ABC/ABC343C.py
@@ -1,24 +1,19 @@
-def check(n: int) -> bool:
-    s = str(n)
-    t = s[::-1]
-    return s == t  # 回文かどうかブール値を返す
+N = int(input())
 
-
-n = int(input())
-
-ans = 0
-i = 1
-while i * i * i <= n:  # 立方数がnを超えたら終了
-    if check(i * i * i):  # 回文なら
-        ans = i * i * i  # ansを更新
-    i += 1
-
+n = 2
+ans = 1
+while n * n * n <= N:  # N以下の立法数を全探索
+    n3 = n * n * n
+    if str(n3) == str(n3)[::-1]:
+        ans = n3  # 回文ならansを更新
+    n += 1
 print(ans)
 
 """
-数学的な気づき系
+数学的な気づき系 + 全探索
+リトライ
 N以下の回分立方数の最大値を求める。
-N 以下の立方数は O(N^(1/3)) 個しかないことを見抜く。
+N 以下の立方数は O(N^(1/3)) 個しかないことを見抜き、全探索する。
 
 https://atcoder.jp/contests/abc343/tasks/abc343_c
 https://chatgpt.com/g/g-p-688d3155796881919ed997146b54eec1-atcoder/c/68f9b584-a80c-8323-b61f-78541874ccc3
