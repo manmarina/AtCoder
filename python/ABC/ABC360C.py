e7d1e0140de6959a0e322d0fc12e68018539408d
manmarina <manmarina@me.com>
2025-11-10 14:05:28 +0900

    360C リトライ 問題文の理解が難解系

--- python/ABC/ABC360C.py
+++ python/ABC/ABC360C.py
@@ -1,33 +1,32 @@
+from collections import defaultdict
+
+
 N = int(input())
 A = list(map(int, input().split()))
 W = list(map(int, input().split()))
 
-# A[i] を 0-index に変換
-A = [a - 1 for a in A]
-print(A)
+# 箱ごとに重みを格納
+dd = defaultdict(list)
+for b, w in zip(A, W):
+    dd[b].append(w)
+print(dd)
 
-# 各値ごとに重みを格納
-tot = [[] for _ in range(N)]
-for i in range(N):
-    tot[A[i]].append(W[i])
-
-# 各グループを昇順にソート
-for v in range(N):
-    tot[v].sort()
-print(tot)
-
-# 各グループで、最大値以外の合計を加算
-res = 0
-for v in range(N):
-    if len(tot[v]) >= 2:
-        res += sum(tot[v][:-1])  # 最後の（最大の）要素を除いて合計
-print(res)
+# 箱の中のグループで、最大値以外の合計を加算
+ans = 0
+for w in dd.values():
+    if len(w) >= 2:
+        w.sort()  # 箱の中のグループを昇順にソート
+        ans += sum(w[:-1])  # 最後の（最大の）要素を除いて合計
+print(ans)
 
 """
 問題文の理解が難解系
+リトライ
 どの箱に何個荷物が入っているのかわかりにくい。
+最初に考えたときよりは、すんなりと問題文の意味がわかった。
+けんちょんのロジックとほぼ同じロジックで完成できた。
 
-けんちょん
+けんちょんの解説
 https://drken1215.hatenablog.com/entry/2024/07/07/211832
 
 https://atcoder.jp/contests/abc360/tasks/abc360_c
