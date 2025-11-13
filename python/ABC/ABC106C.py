a2aed39443ad7ff82a75f48e0328dc2855f01cfd
manmarina <manmarina@me.com>
2025-11-13 20:26:08 +0900

    106C けんちょん 場合分け系

--- python/ABC/ABC106C.py
+++ python/ABC/ABC106C.py
@@ -1,37 +1,29 @@
-S = input()
+S = input().strip()
 K = int(input())
 
-# 先頭に1が続く場合
-i = 0
-if K == 1:  # 1文字しかない時
-    s = S[0]
+ichinum = 0
+for i in range(len(S)):
+    if S[i] == '1':
+        ichinum += 1
+    else:
+        break
 
-elif S[0] == '1':  # 先頭が1の時
-    while i < K and S[i] == '1':  # 1がならぶ間iを伸ばす
-        i += 1
-    if i != K:  # iがK以外の時
-        s = S[i]  # sは1以外の数字
-    else:  # iがkの時（Siがすべて1だった時）
-        s = '1'  # sは1
-
-    if K <= i:  # Kが1がならぶ数以下の時
-        print(1)
-        exit()
-
-else:  # 先頭が1以外の時
-    s = S[0]
-
-print(s)
+if ichinum >= K:
+    print(1)
+else:
+    print(S[ichinum])
 
 """
 場合分け系
+けんちょん
+https://drken1215.hatenablog.com/entry/2018/09/03/224200
 1 だけはそのまま残るが、それ以外は途方もなく多い個数になる。よって、
 先頭の 1 が K 個以上だったら 1
 そうでなかったら、「先頭から見て最初の 1 以外の数」
 が答え
 
-けんちょんの解説
-https://drken1215.hatenablog.com/entry/2018/09/03/224200
+自分の実装よりifが少なくて美しい。
 
 https://atcoder.jp/contests/abc106/tasks/abc106_c
+https://chatgpt.com/g/g-p-688d3155796881919ed997146b54eec1-atcoder/c/6915bf02-8f30-8324-a14b-fb86ce2e63f9
 """
