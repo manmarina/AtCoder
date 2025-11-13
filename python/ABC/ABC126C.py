ad55880c66d0199335dee59951a0d36e8b8ecd99
manmarina <manmarina@me.com>
2025-11-13 23:58:26 +0900

    126C けんちょん 数学的な気づき系

--- python/ABC/ABC126C.py
+++ python/ABC/ABC126C.py
@@ -1,24 +1,23 @@
-import math
-
 N, K = map(int, input().split())
 
+res = 0.0
+for n in range(1, N + 1):
+    tmp = 1.0
+    nn = n
+    while nn < K:  # nnがKを超えない間
+        nn *= 2
+        tmp /= 2.0  # tmpを2で割り続ける
+    res += tmp
 
-ans = 0
-for i in range(1, N + 1):  # すべてのサイコロの目
-    if i >= K:  # 出目がK以上の時
-        ans += 1  # 確率1を加算
-    else:  # そうでないとき
-        exp = math.ceil(math.log(K / i, 2))  # チャッピーの解説を参照
-        ans += pow(0.5, exp)  # コインを振ってexp回連続で表が出る確率
-print(ans / N)  # 総和を求めてからNで割っている
-
+res /= N
+print(f"{res:.10f}")
 
 """
 数学的な気づき系
-入力例1の解説どおりに確率を求める問題。
-
-けんちょんの解説
+けんちょん
 https://drken1215.hatenablog.com/entry/2019/05/19/224500_2
+入力例1の解説どおりに確率を求める問題。
+logを使わない実装。
 
 https://atcoder.jp/contests/abc126/tasks/abc126_c
 https://chatgpt.com/g/g-p-688d3155796881919ed997146b54eec1-atcoder/project
