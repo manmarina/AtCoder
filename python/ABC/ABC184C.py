fab7110360cf4b38f476d61d851ad89c4ba75b3f
manmarina <manmarina@me.com>
2025-11-14 21:34:33 +0900

    184C けんちょん カーソル系 + 場合分け系

--- python/ABC/ABC184C.py
+++ python/ABC/ABC184C.py
@@ -1,43 +1,31 @@
-a, b = map(int, input().split())
-c, d = map(int, input().split())
+def solve():
+    a, b, c, d = map(int, input().split())
+    p = abs(c - a)
+    q = abs(d - b)
+
+    if p == 0 and q == 0:  # 同じ座標は0手
+        print(0)
+        return
+
+    if p == q or p + q <= 3:  # 斜めのライン上か、マンハッタン距離が3未満は1手
+        print(1)
+        return
+
+    # マンハッタン距離が偶数のとき、マンハッタン距離が6以内は2手 abs(p - q) <= 3は理解できない。。
+    if (p + q) % 2 == 0 or p + q <= 6 or abs(p - q) <= 3:
+        print(2)
+        return
+
+    print(3)
 
 
-def count(a, b, c, d):
-    # 同じ座標は0手
-    if a == c and b == d:
-        return 0
-
-    # 斜めのライン上は1手
-    if a + b == c + d or a - b == c - d:
-        return 1
-
-    # マンハッタン距離が3未満は1手
-    if abs(a - c) + abs(b - d) <= 3:
-        return 1
-
-    # マンハッタン距離が偶数のときは2手
-    if (abs(a - c) + abs(b - d)) % 2 == 0:
-        return 2
-
-    # 斜めラインの隣のときは2手
-    if a - b == (c - 1) - d or a - b == c - (d - 1) or \
-            a + b == (c - 1) + d or a + b == c + (d - 1):
-        return 2
-
-    # そうでないとき(マンハッタン距離が奇数）のときは3手
-    return 3
-
-
-print(count(a, b, c, d))
+solve()
 
 """
-カーソル系 + 数学的な気づき系
-WA
-「2手で行ける範囲」 を十分にカバーしきれていないこと。
-
-けんちょんの解説
+カーソル系 + 場合分け系
+けんちょん
 https://drken1215.hatenablog.com/entry/2020/11/22/224600
-偶奇を考える。
+abs(p - q) <= 3が理解できない。。
 
 https://atcoder.jp/contests/abc184/tasks/abc184_c
 https://chatgpt.com/g/g-p-688d3155796881919ed997146b54eec1-atcoder/c/6917197d-a194-8321-8fd9-beb3f2eb173e
