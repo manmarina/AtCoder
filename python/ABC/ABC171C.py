e884bfc0592b58967f6f1aada17e5618788f7492
manmarina <manmarina@me.com>
2025-11-14 15:13:01 +0900

    171C けんちょん 数学的な気づき系

--- python/ABC/ABC171C.py
+++ python/ABC/ABC171C.py
@@ -1,21 +1,12 @@
 N = int(input())
-# print(ord('{'))
 
-hoge = []
+res = ""
 while N > 0:
-    N -= 1  # 毎回1を引くのがポイント
-    hoge.append(N % 26)
+    N -= 1
+    res += chr(ord('a') + (N % 26))
     N //= 26
 
-hoge = hoge[::-1]
-# print(hoge)
-
-fuga = []
-for h in hoge:
-    fuga.append(chr(h + 97))
-
-# print(fuga)
-print(*fuga, sep='')
+print(res[::-1])
 
 """
 数学的な気づき系
@@ -23,7 +14,6 @@ print(*fuga, sep='')
 https://drken1215.hatenablog.com/entry/2020/06/21/225500
 26進数もどき（0が使えない）に変換する問題。
 毎回「N を 1 減らしてから N % 26」を使う処理が重要。
-自分の実装をけんちょんの解説により改良。
 
 https://atcoder.jp/contests/abc171/submissions/me
 https://chatgpt.com/g/g-p-688d3155796881919ed997146b54eec1-atcoder/c/6916c3b7-4884-8320-8fe6-f18d64dfb0bf
