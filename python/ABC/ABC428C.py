9d36fd13f15bc5a6ab22f849bc2b412d4666accc
manmarina <manmarina@me.com>
2025-11-16 12:47:28 +0900

    428C リトライ 場合分け系

--- python/ABC/ABC428C.py
+++ python/ABC/ABC428C.py
@@ -1,61 +1,52 @@
 Q = int(input())
+Query = [list(input().split()) for _ in range(Q)]
+# print(Query)
 
-out = set()  # 不正な')'の出現位置を記録する変数
+out = set()
+stack = []
+score = 0
+for q in Query:
+    if q[0] == '1':
+        _, c = q
+        stack.append(c)
+        if c == ')' and score == 0:
+            out.add(len(stack) - 1)
 
-stack = []  # '(',')'を格納するスタック
-score = 0  # '('は+1、')'は-1する。score==0が"Yes"の必要条件
-for _ in range(Q):
-    q = input().split()
-    num = int(q[0])
-    if num == 1:
-        stack.append(q[1])
+        if c == ')':
+            score -= 1
+        else:  # c == '(':
+            score += 1
 
-        # 不正な')'が出現したら位置をoutに格納
-        if score == 0 and q[1] == ')':
-            out.add(len(stack))
+        # print(stack)
+        # print(out)
+        # print("score:", score)
+        # print()
 
-        if q[1] == '(':
-            score += 1  # '('を追加したときのスコア調整
+    else:  # q[0] == '2':
+        if stack.pop() == ')':
+            score += 1
         else:
-            score -= 1  # ')'を追加したときのスコア調整
-
-    else:  # num == 2
-        if len(stack) in out:  # 不正')'が存在する位置まで戻ってきたらoutから削除
+            score -= 1
+        if len(stack) in out:
             out.remove(len(stack))
-        if stack.pop() == '(':
-            score -= 1  # '('を削除したときのスコア調整
-        else:
-            score += 1  # ')'を削除したときのスコア調整
 
-    if out:  # 不正な’)'が存在する限り"No"
+        # print(stack)
+        # print(out)
+        # print("score:", score)
+        # print()
+
+    if out:
         print("No")
-    elif not stack:
-        print("Yes")
-    elif stack[0] != ')' and score == 0:
+    elif score == 0:
         print("Yes")
     else:
         print("No")
 
-    # print(stack, score, out)
-
 """
 場合分け系
+リトライ
 不正な')'が出現したら位置をoutに格納。
-outが空にならない限り、'('と')'のk図が一致してもYesにならない。
+outが空にならない限り、'('と')'の数が一致してもYesにならない。
 
-コンテスト中は惜しくもWAだったが、
-チャッピーにWAの原因を確認して調整したコードでAC
 https://atcoder.jp/contests/abc428/tasks/abc428_c
-
-鉄則本のp.297 B51もヒントになった。
-
-チャッピーの意見を聞いて作成したオリジナル入力例
-6
-1 (
-1 )
-1 )
-1 (
-2
-2
-
 """
