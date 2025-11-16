f5304346d9e8c7850b51f5d5c37cca10122df342
manmarina <manmarina@me.com>
2025-11-16 14:40:39 +0900

    227C けんちょん 工夫して探索の通り数を減らす全探索

--- python/ABC/ABC227C.py
+++ python/ABC/ABC227C.py
@@ -3,19 +3,19 @@ N = int(sys.stdin.readline())
 
 ans = 0
 a = 1
-while a * a * a <= N:              # a^3 <= N
+while a * a * a <= N:
     b = a
-    while a * b * b <= N:          # a*b^2 <= N
+    while a * b * b <= N:
         cmax = N // (a * b)
-        if cmax >= b:
-            ans += (cmax - b + 1)  # c は b..cmax
+        ans += (cmax - b + 1)  # c は b ~ cmax個ある
         b += 1
     a += 1
 print(ans)
 
 """
-工夫して探索の通り数を減らす「全探索」
-チャッピー
+工夫して探索の通り数を減らす全探索
+けんちょん
+https://drken1215.hatenablog.com/entry/2023/05/19/152200
 「三重ループをしないで、上手に上限を切って二重ループに落とす」問題です。
 
 https://atcoder.jp/contests/abc227/tasks/abc227_c
