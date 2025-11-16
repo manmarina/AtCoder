8d082eedc05c7fa9be9a960e607d3f48399adfda
manmarina <manmarina@me.com>
2025-11-16 11:10:43 +0900

    431C コメント付加 シミュレーション

--- python/ABC/ABC431C.py
+++ python/ABC/ABC431C.py
@@ -7,10 +7,10 @@ B.sort()
 # print(H)
 # print(B)
 
-cnt = 0
+cnt = 0  # 倒れないロボットの数
 k = 0
-for i in range(N):
-    while i + k < M and H[i] > B[i + k]:
+for i in range(N):  # H[i]~H[N-1]まで
+    while i + k < M and H[i] > B[i + k]:  # 頭が体より重い間はkを増やす
         k += 1
     if i + k >= M:
         break
@@ -19,11 +19,16 @@ for i in range(N):
     cnt += 1
 # print(k)
 # print(cnt)
-if cnt >= K:
+if cnt >= K:  # 倒れないロボットがKより多くできた時
     print("Yes")
 else:
     print("No")
 
 """
-AC
+シミュレーション
+頭も体も小さい方から組み合わせしてゆく。
+組み合わせて倒れてしまうときは、次の体を試すのを最後まで繰り返す。
+公式解説のように、頭の小さい方からK個、体の大きい方からK個を組み合わせて考えたほうが簡単そう。
+
+https://atcoder.jp/contests/abc431/tasks/abc431_c
 """
