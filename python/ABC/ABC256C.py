ce7ac4a09b39f183e67557f52ff4802788eaf2d8
manmarina <manmarina@me.com>
2025-11-17 22:37:26 +0900

    256C 解説 工夫して探索の通り数を減らす全探索 DFS版

--- python/ABC/ABC256C.py
+++ python/ABC/ABC256C.py
@@ -1,47 +1,63 @@
+import sys
+sys.setrecursionlimit(10**7)
+
 h1, h2, h3, w1, w2, w3 = map(int, input().split())
+h = [h1, h2, h3]
+w = [w1, w2, w3]
+# print(h)
+# print(w)
 
+# 合計が違う場合は 0
+if sum(h) != sum(w):
+    print(0)
+    exit()
+
+a = [[0] * 3 for _ in range(3)]
 ans = 0
-# 1行目
-for a11 in range(1, h1 - 1):
-    for a12 in range(1, h1 - a11):
-        a13 = h1 - a11 - a12  # 1行3列目
-        if a13 <= 0:
-            continue
 
-        # 2行目
-        for a21 in range(1, h2 - 1):
-            for a22 in range(1, h2 - a21):
-                a23 = h2 - a21 - a22  # 2行3列目
-                if a23 <= 0:
-                    continue
 
-                # 3行目
-                a31 = w1 - a11 - a21
-                a32 = w2 - a12 - a22
-                if a31 <= 0 or a32 <= 0:
-                    continue
+def dfs(ij):
+    global ans
+    i, j = divmod(ij, 3)
 
-                # 3行3列目
-                hol = h3 - a31 - a32
-                ver = w3 - a13 - a23
-                if hol == ver and hol > 0:
-                    a33 = hol
-                    ans += 1
+    # 3x3 すべて埋まった
+    if i == 3:
+        ans += 1
+        return
 
-                    # print("1:", a11, a12, a13)
-                    # print("2:", a21, a22, a23)
-                    # print("3:", a31, a32, a33)
-                    # print()
+    # 最終行（i == 2）のときは列の合計から決め打ち
+    if i == 2:
+        x = w[j] - a[0][j] - a[1][j]
+        if x <= 0:
+            return
+        a[i][j] = x
+        dfs(ij + 1)
 
+    # 最終列（j == 2）のときは行の合計から決め打ち
+    elif j == 2:
+        x = h[i] - a[i][0] - a[i][1]
+        if x <= 0:
+            return
+        a[i][j] = x
+        dfs(ij + 1)
+
+    # それ以外のマスは 1〜30 を全探索
+    else:
+        for x in range(1, 31):
+            a[i][j] = x
+            dfs(ij + 1)
+
+
+dfs(0)
 print(ans)
 
 """
-工夫して探索の通り数を減らす全探索
-写経
+工夫して探索の通り数を減らす全探索 DFS版
+公式
 
 数独・ナンプレ風問題
 3x3 の正の整数マスに行和 H1,H2,H3 と列和 W1,W2,W3 を合わせる「個数を数える」課題です。
 
 https://atcoder.jp/contests/abc256/tasks/abc256_c
-https://chatgpt.com/g/g-p-688d3155796881919ed997146b54eec1-atcoder/c/68de164a-353c-832c-bb68-09af6cf6dbd7
+https://chatgpt.com/g/g-p-688d3155796881919ed997146b54eec1-atcoder/c/691b1ce1-0a44-8321-928e-308788e947cf
 """
