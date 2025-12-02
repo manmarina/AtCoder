cf17654392cfa41bcaf1a60ad3a835e612a0d76f
manmarina <manmarina@me.com>
2025-12-02 14:26:31 +0900

    345C けんちょん 計算量を削減したシミュレーション

--- python/ABC/ABC345C.py
+++ python/ABC/ABC345C.py
@@ -1,24 +1,30 @@
-from collections import Counter
+S = input().strip()
+N = len(S)
 
+# 全ペア数 N*(N-1)/2
+res = N * (N - 1) // 2
 
-S = input()
+can_S = 0
+cnt = [0] * 26
 
-cnt = Counter(S)
-# print(cnt)
+# 文字数カウント
+for c in S:
+    cnt[ord(c) - ord('a')] += 1
 
+# 同じ文字同士のペアを引く & S自身になれるか判定
+for v in range(26):
+    if cnt[v] >= 2:
+        can_S = 1
+    res -= cnt[v] * (cnt[v] - 1) // 2
 
-def nC2(num):
-    return num * (num - 1) // 2
-
-
-ans = nC2(len(S))
-# print(ans)
-
-for v in cnt.values():
-    if v > 1:
-        ans -= (nC2(v) - 1)
-print(ans)
+print(res + can_S)
 
 """
-WA
+計算量を削減したシミュレーション
+けんちょん
+https://drken1215.hatenablog.com/entry/2024/09/04/015546
+操作によってできるものの個数を数え上げる系の問題の最も基本的な問題！
+
+https://atcoder.jp/contests/abc345/tasks/abc345_c
+https://chatgpt.com/g/g-p-688d3155796881919ed997146b54eec1-atcoder/c/692e74dd-60c0-8320-857d-a4db22cff847
 """
