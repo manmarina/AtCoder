4d1f4e0fb43d65ae3050eb00b136b7f15d923425
manmarina <manmarina@me.com>
2025-12-06 23:17:33 +0900

    435C 時間切れ シミュレーション

--- python/ABC/ABC435C.py
+++ python/ABC/ABC435C.py
@@ -2,27 +2,22 @@ N = int(input())
 A = [0] + list(map(int, input().split()))
 # print(A)
 
-i = 1
-while i <= N:
-    hoge = i + A[i] - 1
-
-    if hoge >= N:
-        print(N)
+mx = 0
+for i in range(1, N + 1):
+    ikeru = i + A[i] - 1
+    mx = max(mx, ikeru)
+    if mx == i:
+        print(i)
         exit()
-
-    max_ = i
-    for j in range(i, hoge + 1):
-        fuga = j + A[j] - 1
-        max_ = max(max_, fuga)
-
-    if max_ >= N:
-        print(N)
-        exit()
-
-    if A[max_] == 1:
-        print(max_)
-        exit()
-
-    i = max_
 
 print(N)
+
+"""
+シミュレーション
+時間切れ
+めちゃくちゃ簡単なのにACできなくて悔しい。
+Aiごとに、どこまで倒せるかを計算して最大値を更新。
+もし最大値がiと一致したときにはそこで終了。
+
+https://atcoder.jp/contests/abc435/tasks/abc435_c
+"""
